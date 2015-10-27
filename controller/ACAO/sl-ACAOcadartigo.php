71deecf8b21e5bdb5b38deac78cf66d06dab87e0
marlonsants <marlu_tcu@hotmail.com>
2015-10-26 23:46:53 -0200

    foi adicionada as variaveis que faltavam e feito o upload do arquivo

--- controller/ACAO/sl-ACAOcadartigo.php
+++ controller/ACAO/sl-ACAOcadartigo.php
@@ -12,20 +12,64 @@ $orientador = $_POST['orientador'];
 $descricao = $_POST['descricao'];
 $categoria = $_POST['categoria'];
 
-$tabela = 'sl_artigo';
-$campos = 'titulo, resumo, area, coautor, apresentacao, orientador,descricao,categoria';
-$argumentos  = "'".$titulo."','".$resumo."', '".$area."', '".$coautor."', '".$apresentacao."', '".$orientador."','".$descricao."','".$categoria."' ";
+// informações do artigo 
 
 
-$rp = insert($tabela,$campos,$argumentos);
+$arq = $_POST['arq'];
+$tipo = $_POST['tipo'];
+$tamanho = $_POST['tamanho'];
+$nametemp = $_POST['nametemp'];
+$arqerro = $_POST['arqerro'];
+$tipodef = "application/pdf";
+$tamnhodef = 1024*1024*2; //Definir tamanho maximo do arquivo.
+
+
+
+if(isset($arq)){
+			if($tipo != $tipodef){
+				echo ("Tipo de Arquivo não suportado! <br/> Verifique o erro e tente novamente.");
+			}else if ($tamanho > $tamnhodef){
+				echo ("O arquivo excede o tamnho limite! <br/> Verifique o erro e tente novamente.");
+			}else if ($tamanho <= $tamnhodef && $tipo = $tipodef && $arqerro == 0) {
+				echo("<div class='alert-success'><center> Sucesso</center></div>");
+
+			    $extensao = explode(".", $arq);
+			      // Define o novo nome do arquivo usando um UNIX TIMESTAMP
+			     $id = uniqid(rand()) .'.'. $extensao[1];
+
+				$pasta = '../../uploads/'.$id;
+			    $upload = move_uploaded_file($nametemp, $pasta);
+			    $nomeartigo = explode(".", $id);
+			    echo $nomeartigo[0];
+
+			    session_start();
+			    $email = $_SESSION['usuario'];
+			    
+			    $idartigo = $nomeartigo[0];
+			    // variaveis do insert
+				$tabela = 'sl_artigo';
+				$campos = 'titulo, resumo, area, coautor, apresentacao, orientador,descricao,categoria,idartigo';
+				$argumentos  = "'".$titulo."','".$resumo."', '".$area."', '".$coautor."', '".$apresentacao."', '".$orientador."','".$descricao."','".$categoria."', '".$idartigo."' ";
+
+				$rp = insert($tabela,$campos,$argumentos);
+    			updatemysql("idartigo = '".$nomeartigo[0]."' ",'sl_cadusu',"email = '".$email."' ");
+    		//header("Location:../view/fim.php?artigo?titulo="+env.titulo+"?enviado" );
+			}
+			
+		}
 
 if($rp){
-	session_start();
+	
 	updatemysql('publicado = 1','sl_cadusu',"email = '".$_SESSION['usuario']."' ");
+	
 }
 
 echo "</br>";      
 echo json_encode($rp);	
+
+   
+
+
 
    
 	   
@@ -33,3 +77,5 @@ echo json_encode($rp);
 
 
 ?>
+
+
