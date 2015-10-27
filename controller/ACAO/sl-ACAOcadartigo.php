96de945f2f74addfa1f2d52febaaa25977b6ad02
marlonsants <marlu_tcu@hotmail.com>
2015-10-27 14:52:25 -0200

    comentei o código e coloquei algumas impressão de debug

--- controller/ACAO/sl-ACAOcadartigo.php
+++ controller/ACAO/sl-ACAOcadartigo.php
@@ -13,8 +13,6 @@ $descricao = $_POST['descricao'];
 $categoria = $_POST['categoria'];
 
 // informações do artigo 
-
-
 $arq = $_POST['arq'];
 $tipo = $_POST['tipo'];
 $tamanho = $_POST['tamanho'];
@@ -23,36 +21,47 @@ $arqerro = $_POST['arqerro'];
 $tipodef = "application/pdf";
 $tamnhodef = 1024*1024*2; //Definir tamanho maximo do arquivo.
 
-
-
+print_r($_POST);
+// se arquivo existir e for diferente de vazio
 if(isset($arq)){
 			if($tipo != $tipodef){
 				echo ("Tipo de Arquivo não suportado! <br/> Verifique o erro e tente novamente.");
 			}else if ($tamanho > $tamnhodef){
 				echo ("O arquivo excede o tamnho limite! <br/> Verifique o erro e tente novamente.");
-			}else if ($tamanho <= $tamnhodef && $tipo = $tipodef && $arqerro == 0) {
-				echo("<div class='alert-success'><center> Sucesso</center></div>");
+			}else if ($tamanho <= $tamnhodef && $tipo == $tipodef && $arqerro == 0) {
+				echo("passou no primeiro teste </br> ");
 
 			    $extensao = explode(".", $arq);
-			      // Define o novo nome do arquivo usando um UNIX TIMESTAMP
-			     $id = uniqid(rand()) .'.'. $extensao[1];
+			     // Define o novo nome do arquivo usando um UNIX TIMESTAMP
+			    $id = uniqid(rand()) .'.'. $extensao[1];
 
 				$pasta = '../../uploads/'.$id;
-			    $upload = move_uploaded_file($nametemp, $pasta);
-			    $nomeartigo = explode(".", $id);
-			    echo $nomeartigo[0];
+				if($arqerro == 0){
+			    $upload = move_uploaded_file($nametemp,$pasta);
+			    echo 'upload realizado   ';
+				}else{
+					echo 'erro ao fazer o upload   ';
+				}
+			    if($upload){
+			    	echo 'moveupload feito com sucesso   ';
+			    }else{
+			    	echo 'ocorreu um erro no moveupload'.'  '.$upload.'</br>  ';
+			    	
+			    }
+			    $idart = explode(".", $id);
+			    echo "id do artigo"."  ".$idart[0];
 
 			    session_start();
 			    $email = $_SESSION['usuario'];
 			    
-			    $idartigo = $nomeartigo[0];
+			    $idartigo = $idart[0];
 			    // variaveis do insert
 				$tabela = 'sl_artigo';
 				$campos = 'titulo, resumo, area, coautor, apresentacao, orientador,descricao,categoria,idartigo';
 				$argumentos  = "'".$titulo."','".$resumo."', '".$area."', '".$coautor."', '".$apresentacao."', '".$orientador."','".$descricao."','".$categoria."', '".$idartigo."' ";
 
 				$rp = insert($tabela,$campos,$argumentos);
-    			updatemysql("idartigo = '".$nomeartigo[0]."' ",'sl_cadusu',"email = '".$email."' ");
+    			updatemysql("idartigo = '".$idartigo."' ",'sl_cadusu',"email = '".$email."' ");
     		//header("Location:../view/fim.php?artigo?titulo="+env.titulo+"?enviado" );
 			}
 			
