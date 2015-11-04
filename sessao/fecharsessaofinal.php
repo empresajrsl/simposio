559fee5d44a2a83d706cfa1162a2250ec32bb3f9
marlonsants <marlu_tcu@hotmail.com>
2015-11-04 09:30:49 -0200

    retirei o id do artigo do get

--- sessao/fecharsessaofinal.php
+++ sessao/fecharsessaofinal.php
@@ -1,8 +1,7 @@
 <?php
 	session_start(); // Inicia a sessão
 	session_destroy(); // Destrói a sessão limpando todos os valores salvos
-	if(isset($_GET['id'])){
-	$id = $_GET['id'];	
-	header("Location: ../view/fim.php?id=".$id); exit; // Redireciona o visitante
-	}
+	
+	header("Location: ../view/fim.php"); exit; // Redireciona o visitante
+	
 ?>
