315dd7f2c428b4aea8d67a45d6e7c7574bbfd349
marlonsants <marlu_tcu@hotmail.com>
2015-11-12 01:28:50 -0200

    funcao que busca os dados com base no cpf

--- controller/ACAO/buscacoautor.php
+++ controller/ACAO/buscacoautor.php
@@ -3,13 +3,22 @@
 include ('../funcoes/conexao.php');
 include ('../funcoes/funcoesmysql.php');
 
-$cpfcoautor1 = $_POST['cpfcoautor1'];
+$cpf = $_POST['cpf'];
+$coautor = $_POST['coautor'];
 
-$rp = select('*','sl_cadusu',"cpf = '".$cpfcoautor1."' ");
+$rp = select('*','sl_cadusu',"cpf = '".$cpf."' ");
 
-$dados = $rp[0];
 
-echo json_encode($dados);
+$dados = $rp;
+
+
+
+	
+
+
+
+
+echo json_encode($dados[0]);
 
 
 
