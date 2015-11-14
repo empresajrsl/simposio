9785850ec87a9578d98d12ff28f7f0e52058f701
Anderson Lima <andersoncardosolima0@gmail.com>
2015-11-14 13:25:11 -0200

    Adicionei o banner e o icone

--- view/enviaemail.php
+++ view/enviaemail.php
@@ -40,21 +40,23 @@ echo ($mensagem);*/
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/tooltip.js"></script>
     <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
-	<title>Confirma E-mail</title>
+	<title>Confirmação por E-mail</title>
+    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
 </head>
 <body>
 	<div class="container">
-		<!--Banner-->
+    	
+        <!--Banner-->
         <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
-            <div class="row">
+            <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
                 <div class="col-md-12 col-xs-12 col-lg-12">
-                    <div class="col-md-2 col-xs-3 col-lg-2">
-                        <img class="col-md-12 col-xs-12 col-lg-12" src="../images/SGAGRO LOGO.png">
-                        <img class="col-md-12 col-xs-12 col-lg-12" src="../images/unesp.jpg" style="margin-top: 25%">
+                    <div class="col-md-4 col-xs-4 col-lg-4">
+                        <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
+                        <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
                     </div>
-                    <h1 class="col-md-8 col-xs-6 col-lg-8">
-                        <center style="margin-top: 5%"><i style="font-family: 'Plantagenet Cherokee';"><b>E-Mail</b></i></center>
-                    </h1>
+                    <div class="col-md-6 col-xs-6 col-lg-6">
+                        <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Confirmação por E-mail</b></i></center></h1>
+                    </div>
                 </div>
             </div>
         </div>
