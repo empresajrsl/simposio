8a093aaf81f5b540b0078d4e9de75c94f06abcaf
kraoolyh <anderson.souza0909@gmail.com>
2015-11-16 10:46:47 -0200

    Inserçoes na tabela sl_grupo

--- controller/ACAO/sl-ACAOcadartigo.php
+++ controller/ACAO/sl-ACAOcadartigo.php
@@ -14,43 +14,83 @@ $categoria = $_POST['categoria'];
 session_start();
 $email = $_SESSION['usuario'];
 // recebe as variaveis realcionadas a coautor via post
-$nomecoautor1 = $_POST['nomecoautor1'];
-$nomecoautor2 = $_POST['nomecoautor2'];
-$nomecoautor3 = $_POST['nomecoautor3'];
-$nomecoautor4 = $_POST['nomecoautor4'];
-
-$snomecoautor1 = $_POST['snomecoautor1'];
-$snomecoautor2 = $_POST['snomecoautor2'];
-$snomecoautor3 = $_POST['snomecoautor3'];
-$snomecoautor4 = $_POST['snomecoautor4'];
-
+if(isset($_POST['cpfcoautor1'])){
 $cpfcoautor1 = $_POST['cpfcoautor1'];
+}
+
+if(isset($_POST['cpfcoautor2'])){
 $cpfcoautor2 = $_POST['cpfcoautor2'];
+}
+
+if(isset($_POST['cpfcoautor3'])){
 $cpfcoautor3 = $_POST['cpfcoautor3'];
+}
+
+if(isset($_POST['cpfcoautor4'])){
 $cpfcoautor4 = $_POST['cpfcoautor4'];
+}
 
 if($coautor != "0"){
 	if($coautor == "1"){
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor1."','".$snomecoautor1."','".$cpfcoautor1."','".$email."' ");
+		$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
+		$idaut1 =  $idautor1[0]['id_usuario'];
+
+		$cpfautor1 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor1."'"); 
+		$cpfaut1 =  $cpfautor1[0]['id_usuario'];
+		insert('sl_grupo','id_usuario1, id_usuario2',"'".$idaut1."','".$cpfaut1."'");
+
 	}
 	if($coautor == "2"){
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor1."','".$snomecoautor1."','".$cpfcoautor1."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor2."','".$snomecoautor2."','".$cpfcoautor2."','".$email."' ");
+		$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
+		$idaut1 =  $idautor1[0]['id_usuario'];
+
+		$cpfautor1 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor1."'"); 
+		$cpfcoaut1 =  $cpfautor1[0]['id_usuario'];
+	
+		$cpfautor2 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor2."'"); 
+		$cpfcoaut2 =  $cpfautor2[0]['id_usuario'];
+
+		
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."'");
 	}
 	if($coautor == "3"){
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor1."','".$snomecoautor1."','".$cpfcoautor1."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor2."','".$snomecoautor2."','".$cpfcoautor2."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor3."','".$snomecoautor3."','".$cpfcoautor3."','".$email."' ");
+
+		$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
+		$idaut1 =  $idautor1[0]['id_usuario'];
+
+		$cpfautor1 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor1."'"); 
+		$cpfcoaut1 =  $cpfautor1[0]['id_usuario'];
+	
+		$cpfautor2 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor2."'"); 
+		$cpfcoaut2 =  $cpfautor2[0]['id_usuario'];
+
+		$cpfautor3 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor3."'"); 
+		$cpfcoaut3 =  $cpfautor3[0]['id_usuario'];
+
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4', "'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."'");
 	}
 	if($coautor == "4"){
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor1."','".$snomecoautor1."','".$cpfcoautor1."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor2."','".$snomecoautor2."','".$cpfcoautor2."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor3."','".$snomecoautor3."','".$cpfcoautor3."','".$email."' ");
-		insert('sl_coautor','nome,sobrenome,cpf,autorprincipal'," '".$nomecoautor4."','".$snomecoautor4."','".$cpfcoautor4."','".$email."' ");
+
+		$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
+		$idaut1 =  $idautor1[0]['id_usuario'];
+
+		$cpfautor1 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor1."'"); 
+		$cpfcoaut1 =  $cpfautor1[0]['id_usuario'];
+	
+		$cpfautor2 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor2."'"); 
+		$cpfcoaut2 =  $cpfautor2[0]['id_usuario'];
+
+		$cpfautor3 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor3."'"); 
+		$cpfcoaut3 =  $cpfautor3[0]['id_usuario'];
+	
+		$cpfautor4 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor4."'"); 
+		$cpfcoaut4 =  $cpfautor4[0]['id_usuario'];
+
+		echo $cpfcoaut4;
+
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4,id_usuario5',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."','".$cpfcoaut4."'");
 	}
 }
-	
-echo $nomecoautor1;
 	
  // variaveis do insert
 				$tabela = 'sl_artigo';
