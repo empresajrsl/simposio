4e8b017f8c62c954b79f8d44e838bfbd1f829794
marlonsants <marlu_tcu@hotmail.com>
2015-11-16 14:14:50 -0200

    inclui a inserção do id do artigo na tabela e criei um else caso não existam coautores insere só autor 1

--- controller/ACAO/sl-ACAOcadartigo.php
+++ controller/ACAO/sl-ACAOcadartigo.php
@@ -30,6 +30,25 @@ if(isset($_POST['cpfcoautor4'])){
 $cpfcoautor4 = $_POST['cpfcoautor4'];
 }
 
+// variaveis do insert
+				$tabela = 'sl_artigo';
+				$campos = 'titulo, resumo, area, coautor, apresentacao, orientador,descricao,categoria,email';
+				$argumentos  = "'".$titulo."','".$resumo."', '".$area."', '".$coautor."', '".$apresentacao."', '".$orientador."','".$descricao."','".$categoria."','".$email."'";
+
+				$rp = insert($tabela,$campos,$argumentos);
+if($rp){
+	 
+	updatemysql('publicado = 1','sl_cadusu',"email = '".$_SESSION['usuario']."' ");
+	
+}
+
+$rp = select('id_artigo','sl_artigo',"  titulo = '".$titulo."' and resumo = '".$resumo."' and email = '".$email."' and orientador = '".$orientador."'   ");
+
+
+$idtbartigo = $rp[0];
+echo json_encode($idtbartigo['id_artigo']);
+
+
 if($coautor != "0"){
 	if($coautor == "1"){
 		$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
@@ -37,7 +56,7 @@ if($coautor != "0"){
 
 		$cpfautor1 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor1."'"); 
 		$cpfaut1 =  $cpfautor1[0]['id_usuario'];
-		insert('sl_grupo','id_usuario1, id_usuario2',"'".$idaut1."','".$cpfaut1."'");
+		insert('sl_grupo','id_usuario1, id_usuario2,id_artigo',"'".$idaut1."','".$cpfaut1."',".$idtbartigo['id_artigo']." ");
 
 	}
 	if($coautor == "2"){
@@ -51,7 +70,7 @@ if($coautor != "0"){
 		$cpfcoaut2 =  $cpfautor2[0]['id_usuario'];
 
 		
-		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."'");
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_artigo',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."',".$idtbartigo['id_artigo']." ");
 	}
 	if($coautor == "3"){
 
@@ -67,7 +86,7 @@ if($coautor != "0"){
 		$cpfautor3 = select('id_usuario', 'sl_cadusu', "cpf= '".$cpfcoautor3."'"); 
 		$cpfcoaut3 =  $cpfautor3[0]['id_usuario'];
 
-		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4', "'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."'");
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4,id_artigo', "'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."',".$idtbartigo['id_artigo']." ");
 	}
 	if($coautor == "4"){
 
@@ -88,21 +107,15 @@ if($coautor != "0"){
 
 		echo $cpfcoaut4;
 
-		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4,id_usuario5',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."','".$cpfcoaut4."'");
+		insert('sl_grupo','id_usuario1, id_usuario2,id_usuario3,id_usuario4,id_usuario5,id_artigo',"'".$idaut1."','".$cpfcoaut1."','".$cpfcoaut2."','".$cpfcoaut3."','".$cpfcoaut4."',".$idtbartigo['id_artigo']." ");
 	}
+}else{
+	$idautor1 = select('id_usuario', 'sl_cadusu', "email= '".$email."'"); 
+	$idaut1 =  $idautor1[0]['id_usuario'];
+	insert('sl_grupo','id_usuario1,id_artigo',"'".$idaut1."',".$idtbartigo['id_artigo']." ");
 }
 	
- // variaveis do insert
-				$tabela = 'sl_artigo';
-				$campos = 'titulo, resumo, area, coautor, apresentacao, orientador,descricao,categoria,email';
-				$argumentos  = "'".$titulo."','".$resumo."', '".$area."', '".$coautor."', '".$apresentacao."', '".$orientador."','".$descricao."','".$categoria."','".$email."'";
-
-				$rp = insert($tabela,$campos,$argumentos);
-if($rp){
-	 
-	updatemysql('publicado = 1','sl_cadusu',"email = '".$_SESSION['usuario']."' ");
-	
-}
+ 
 ?>
 
 echo "</br>";      
