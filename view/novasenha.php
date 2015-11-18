0e09dd3789b3e2396e3189b2127fceebf8857d6a
marlonsants <marlu_tcu@hotmail.com>
2015-11-18 11:14:36 -0200

    tela onde o usuario informa a nova senha

--- view/novasenha.php
+++ view/novasenha.php
@@ -1,127 +1,150 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
-    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
-    <script src="../js/jquery-2.1.1.min.js"></script>
-    <script src="../js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="../plugin/mask/jquery.mask.js"></script>
-    <script src="../js/tooltip.js"></script>
-    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
-    <title>Criar nova senha</title>
-    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+<?php header ('Content-type: text/html; charset=UTF-8'); ?>
+<?php 
+	
+	if(isset($_GET['key'])){
+		$sigla =  $_GET['key'];
+		$key = base64_encode($sigla);
+		if( $key != 'sigmajrsaoluis'){
+		echo '<h2> Para que possa alterar sua senha é necessário clicar no link, enviado para o seu email !!! </h2>';
+
+		return;
+		}
+	}else{
+		echo '<h2> Para que possa alterar sua senha é necessário clicar no link, enviado para o seu email !!! </h2>';
+		return;
+
+	}
+?>
+<!DOCTYPE html>
+<html lang="en-us" id="lock-page">
+	<head>
+		<meta charset="utf-8">
+		<title>SGAGRO</title>
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+		
+		 <!-- Bootstrap Core CSS -->
+    	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
+	    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
+	    <script src="../js/jquery-2.1.1.min.js"></script>
+	    <script src="../js/bootstrap.min.js"></script>
+	    <script src="../js/tooltip.js"></script>
+	    <script type="text/javascript" src="../plugin/mask/jquery.mask.js"></script>
+	    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
+	    <title>Tela de Cadastro</title>
+	    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+
 </head>
-<body>
-  <div class="container">
-    <!--Banner-->
-    <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
-        <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
-            <div class="col-md-12 col-xs-12 col-lg-12">
-                <div class="col-md-4 col-xs-4 col-lg-4">
-                    <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
-                    <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
-                </div>
-                <div class="col-md-6 col-xs-6 col-lg-6">
-                    <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Nova Senha</b></i></center></h1>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!--Fim Banner-->
+	
+<body style="overflow:hidden">
 
-		<form class="jumbotron" id="novasenha" name="novasenha" method="post" action="../controller/ACAO/sl-ACAOcriaNsenha.php">
+			<!-- MAIN CONTENT -->
+	<div class="container">
+			<div class="row" >
+					<!-- div principal do fomulario -->
+					<div class="row">
+						<h2> Criar nova senha </h2>
+					</div>
+			
+					<!-- verifica se o php retornou mesangem  -->
+					<?php  
+					// verifica se ocorreu erro
+					if(isset($_GET['erro']))
+					{ 
+					// armazena o valor do erro 	
+						$erro = $_GET['erro'];
+					// se erro for = 1 email incorreto	
+						if($erro == 3)
+					{
+						echo('<h5 style="color: black; font-size:15px">O e-mail está incorreto verifique e tente novamente </h5>');
+					}
+					// se não cpf incorreto
+					else if($erro == 2)
+					{
+						echo('<h5 style="color: black; font-size:15px"> Você não está cadastrado ou tem pendencias em seu cadastro</h5>');
+					}
+					// fim do isset
+					}
+					else{
+					// verifica se teve sucesso
+					if (isset($_GET['msg']))
+					{
+						$msg = $_GET['msg'];
+					if ($msg == 1)
+					{
+						echo('<h5 style="color: black; font-size:15px"> Atualização da senha feita com sucesso </h5>');
+					}
+					// fim do isset msg
+					}
+					// fim do else
+					}
+					?>
+				<!-- fim do php -->
+				
+				<div class="row">
+					<form id="novasenha" method="post" action="../controller/ACAO/sl-ACAOcriaNsenha.php">
+						<div class="row">
+							<div class="col-md-4">
+								<label > E-mail </label>
+									<input class="form-control" type="input" name="email" id="email" >
+								<label > Senha </label>
+									<input class="form-control" type="password" name="senha1" id="senha1" > 
+								<label >Digite a senha novamente </label>
+									<input class="form-control" type="password" name="senha2" id="senha2" >
+							</div>
+						</div>	
+						<div class="row">
+							<div class="col-md-4">
+								<button type="button"  name="submit" id="submit"  class="btn btn-primary"> Salvar </button>
+							</div>
+						</div>		 
+					</form>
+					<!-- fim do form -->
+				<div class="row">
+				
+		
+		
+			</div>
+	<!-- fecha container -->
+	</div>
 
-        <div class="row">
-          <div class="col-md-12 col-xs-12 col-lg-12">
-            <?php 
-                if(isset($_GET['msg'])){
-                  $msg = $_GET['msg'];
-                  if(isset($_GET['erro'])){
-                  $erro = $_GET['erro'];
-                  }
-                
-                
-                if($msg == 1){
-                  echo '<div class="alert-success"><center>Senha atualizada com sucesso!<center></div>';
-                }
-                else if($msg == 2){
-                  echo '<div class="alert-danger"><center>Os dados fornecidos não conferem.<center></div>';
-                }
-                else if($msg == 3){
-                  echo '<div class="alert-danger"><center>Erro ao efetuar alterações. <br/>'. $erro .' <center></div>';
-                }
-              }
-            ?>  
-        </div>
-      </div>
-        
-        <div class="row">
-        <div class="col-md-12 col-xs-12 col-lg-12">
-          <div class="col-md-4 col-md-offset-4">
-            <label> C.P.F. </label></br>
-            <input type="input" name="cpf" id="cpf" class="form-control"></br>
-          </div>
-        </div>
-        </div>
-        
-        <div class="row">
-        <div class="col-md-12 col-xs-12 col-lg-12">
-          <div class="col-md-4 col-md-offset-2">
-            <label> E-mail </label></br>
-            <input type="input" name="email" id="email" class="form-control"></br>
-          </div>
-        
-          <div class="col-md-4">
-            <label>Confirmar e-mail </label></br>
-            <input type="input" name="confirma_email" id="confirma_email" class="form-control"></br>
-          </div>
-        </div>
-        </div>
+			
 
-        <div class="row">
-        <div class="col-md-12 col-xs-12 col-lg-12">
-          <div class="col-md-4 col-md-offset-2">
-            <label>Nova senha </label></br>
-            <input type="password" name="senha1" id="senha1" class="form-control"></br>
-          </div>
-        
 
-          <div class="col-md-4">
-            <label>Confirme a nova senha</label></br>
-            <input type="password" name="senha2" id="senha2" class="form-control"></br>
-          </div>
-        </div>
-        </div>
-      
 
-      </br>
-      
-        
-          <div class="row">
-            <div class="col-md-12 col-xs-12 col-lg-12">
-            <button type="button" name="voltar" id="voltar" class="btn btn-primary col-md-2 col-md-offset-3 col-xs-2 col-xs-offset-3 col-lg-2 col-lg-offset-3">Voltar</button>            
-            <button type="submit" name="salvar" id="salvar" class="btn btn-success col-md-2 col-md-offset-2 col-xs-2 col-xs-offset-2 col-lg-2 col-lg-offset-2">Salvar</button>
-          </div>
-          </div>
-          
-        
-      
-		</form>
-	</div>	
-  <script type="text/javascript">
-    $(document).on('click','#voltar', function(){
-      location.href="../index.php";
 
-    });
-  </script>
-<script>
-    $(document).ready( function(){
-        // mascara 
-        $("#cpf").mask("999.999.999-99"); 
-    });
-</script>
-	<!-- função para enviar post via ajax  e validar o form -->
-	// <script type="text/javascript" src="../js/scripts/envionovasenha.js"></script>
+	<!-- função para validar a senha -->
+	<script type="text/javascript">
 
+	 // ler todo o documento em busca das informações
+	 	$( document ).ready(function() {
+
+	// chamar a função quando clica no botão submit 
+			$(document).on("click","#submit",function(){	
+	// armazena os valores de senha e senha 2 em variaveis temporarias
+				var senha1 = $("#senha1").val();
+				var senha2 = $("#senha2").val();
+		
+	// se as senhas forem identicas
+					if( senha1 == senha2)
+					{
+		
+						$('#novasenha').submit();
+
+					}
+	// caso as senhas não sejam identicas 
+					else{
+							alert("Erro as senhas não correspondem !!!");
+							 $("#senha1").val('');
+							 $("#senha2").val('');
+
+						}
+
+	// fecha funçao on.click
+			});
+	// fecha o ready	
+		});
+	</script>
+		
 </body>
+</html>
