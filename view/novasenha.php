740c1423137426ff1cc90aae1d42a1b75401d922
Anderson Lima <andersoncardosolima0@gmail.com>
2015-11-18 16:00:05 -0200

    Adicionei as modificações visuais e a responsividade

--- view/novasenha.php
+++ view/novasenha.php
@@ -15,37 +15,52 @@
 
 	}
 ?>
-<!DOCTYPE html>
+
 <html lang="en-us" id="lock-page">
-	<head>
-		<meta charset="utf-8">
-		<title>SGAGRO</title>
-		
-		<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-		
-		 <!-- Bootstrap Core CSS -->
-    	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
-	    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
-	    <script src="../js/jquery-2.1.1.min.js"></script>
-	    <script src="../js/bootstrap.min.js"></script>
-	    <script src="../js/tooltip.js"></script>
-	    <script type="text/javascript" src="../plugin/mask/jquery.mask.js"></script>
-	    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
-	    <title>Tela de Cadastro</title>
-	    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+<head>
+<meta charset="utf-8">
+<title>SGAGRO</title>
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+
+ <!-- Bootstrap Core CSS -->
+<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
+<link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
+<script src="../js/jquery-2.1.1.min.js"></script>
+<script src="../js/bootstrap.min.js"></script>
+<script src="../js/tooltip.js"></script>
+<script type="text/javascript" src="../plugin/mask/jquery.mask.js"></script>
+<script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
+<link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
 
 </head>
 	
-<body style="overflow:hidden">
-
-			<!-- MAIN CONTENT -->
+<body>
 	<div class="container">
-			<div class="row" >
-					<!-- div principal do fomulario -->
+		<!--Banner-->
+	    <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
+	        <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
+	            <div class="col-md-12 col-xs-12 col-lg-12">
+	                <div class="col-md-4 col-xs-4 col-lg-4">
+	                    <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
+	                    <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
+	                </div>
+	                <div class="col-md-6 col-xs-6 col-lg-6">
+	                    <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Nova Senha</b></i></center></h1>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <!--Fim Banner-->
+
+	    <!-- div principal do fomulario -->
+	    <div class="jumbotron">
+	    	<div class="row">
+	    		<div class="col-md-12 col-xs-12 col-lg-12">
 					<div class="row">
-						<h2> Criar nova senha </h2>
+						<center><h2>Criar nova senha</h2></center>
 					</div>
-			
+
 					<!-- verifica se o php retornou mesangem  -->
 					<?php  
 					// verifica se ocorreu erro
@@ -79,39 +94,53 @@
 					// fim do else
 					}
 					?>
-				<!-- fim do php -->
-				
-				<div class="row">
-					<form id="novasenha" method="post" action="../controller/ACAO/sl-ACAOcriaNsenha.php">
-						<div class="row">
-							<div class="col-md-4">
-								<label > E-mail </label>
-									<input class="form-control" type="input" name="email" id="email" >
-								<label > Senha </label>
-									<input class="form-control" type="password" name="senha1" id="senha1" > 
-								<label >Digite a senha novamente </label>
-									<input class="form-control" type="password" name="senha2" id="senha2" >
+					<!-- fim do php -->
+
+					<!--começo form-->
+					<div class="row">
+						<div class="col-md-12 col-xs-12 col-lg-12">
+						<form class="col-md-12 col-xs-12 col-lg-12" id="novasenha" method="post" action="../controller/ACAO/sl-ACAOcriaNsenha.php">
+							<div class="row">
+								<div class="col-md-4 col-md-offset-4">
+									<label > E-mail </label>
+									<input class="form-control" type="input" name="email" id="email">
+								</div>
 							</div>
-						</div>	
-						<div class="row">
-							<div class="col-md-4">
-								<button type="button"  name="submit" id="submit"  class="btn btn-primary"> Salvar </button>
+
+							</br>
+
+							<div class="row">
+								<div class="col-md-4 col-md-offset-4">
+									<label > Senha </label>
+									<input class="form-control" type="password" name="senha1" id="senha1">
+								</div>
 							</div>
-						</div>		 
-					</form>
-					<!-- fim do form -->
-				<div class="row">
-				
-		
-		
+
+							</br>
+
+							<div class="row">
+								<div class="col-md-4 col-md-offset-4">
+									<label>Digite a senha novamente</label>
+									<input class="form-control" type="password" name="senha2" id="senha2">
+								</div>
+							</div>
+
+							</br>
+
+							<div class="row">
+								<div class="col-md-4 col-md-offset-4">
+									<button type="button"  name="submit" id="submit"  class="btn btn-primary col-md-12 col-xs-12">Salvar</button>
+								</div>
+							</div>		 
+						</form>
+						</div>
+					</div>
+					<!-- fim form -->
+				</div>
 			</div>
-	<!-- fecha container -->
+		</div>
+
 	</div>
-
-			
-
-
-
 
 	<!-- função para validar a senha -->
 	<script type="text/javascript">
