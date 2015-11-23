606abb801464bb4a735fca9a83bd37fd98ef96b8
marlonsants <marlu_tcu@hotmail.com>
2015-11-22 23:59:24 -0200

    não deixa ousuario voltar na pagina e se cadastrar novamente

--- view/confirma_cadastro.php
+++ view/confirma_cadastro.php
@@ -125,58 +125,86 @@ $cargo = $_POST['cargo'];
 
 	</div>
 
+
+	
+
 	<script type="text/javascript">
-$(document).ready(function(){
-			$(document).on('click','#continuar1',function(){ 
-				env = {};
-				var senha1 = "<?php echo $senha; ?>"
-                env.senha = senha1 ;
-                env.cargo = "<?php echo $cargo; ?>" ;
-                env.nome = "<?php echo $nome; ?>" ;
-                env.sobrenome = "<?php echo $sobrenome; ?>" ;
-                env.nascimento = "<?php echo $nascimento; ?>" ;
-                env.sexo = "<?php echo $sexo; ?>" ;
-                env.pais = "<?php echo $pais; ?>" ;
-                env.cpf = "<?php echo $cpf; ?>" ;
-                env.endereco = "<?php echo $endereco; ?>" ;
-                env.cidade = "<?php echo $cidade; ?>" ;
-                env.estado = "<?php echo $estado; ?>" ;
-                env.instituicao = "<?php echo $instituicao; ?>" ;
-				env.email = "<?php echo $email; ?>" ;
-                env.telefone = "<?php echo $telefone; ?>" ;
-                env.contato = "<?php echo $contato; ?>" ;
-                env.numero = "<?php echo $numero; ?>" ;
-                env.complemento = "<?php echo $complemento; ?>" ;
-                env.cep = "<?php echo $cep; ?>" ;
-                env.bairro = "<?php echo $bairro; ?>" ;
-                env.tipocadastro = "<?php echo $tipocadastro; ?>" ;
-                
+		$(document).ready(function(){
+			$(document).on('click','#continuar1',function(){
+				var verifica = {}; 
+				verifica.email = "<?php echo $email; ?>" ;
+				var errocadastro = 0;
+
 				$.ajax({
 			            type: "POST",
-			            url: "../controller/ACAO/sl-ACAOcadusu.php",
-			            data: env,
-			            dataType : 'html',
+			            url: "../controller/ACAO/sl-ACAOverificaemail.php",
+			            data: verifica,
+			            dataType : 'json',
 			            success: function(data){
-			                console.log('sucesso'+data)
-			                if(data.length > 0){
-			                	var form = {};
-		        				form.email = $('#email').val();
-		                         
-		    					window.location.href ="enviaemail.php?email="+env.email;
+			               
+			               		if(verifica.email == data['email']){
+			               		
+			                	alert('este email ja foi cadastro no sistema, entre no link enviado para '+verifica.email+', para confirmar seu cadastrado');
+			                	window.location.href ="../index.php";
+			                	
+			                	
 			                }
+			                
 			            }, error: function(data) {
 			                console.log(data);
-			                alert("Erro, Houve uma falha ao salvar o registro!");
+			               	
+			               	env = {};
+								var senha1 = "<?php echo $senha; ?>"
+				                env.senha = senha1 ;
+				                env.cargo = "<?php echo $cargo; ?>" ;
+				                env.nome = "<?php echo $nome; ?>" ;
+				                env.sobrenome = "<?php echo $sobrenome; ?>" ;
+				                env.nascimento = "<?php echo $nascimento; ?>" ;
+				                env.sexo = "<?php echo $sexo; ?>" ;
+				                env.pais = "<?php echo $pais; ?>" ;
+				                env.cpf = "<?php echo $cpf; ?>" ;
+				                env.endereco = "<?php echo $endereco; ?>" ;
+				                env.cidade = "<?php echo $cidade; ?>" ;
+				                env.estado = "<?php echo $estado; ?>" ;
+				                env.instituicao = "<?php echo $instituicao; ?>" ;
+								env.email = "<?php echo $email; ?>" ;
+				                env.telefone = "<?php echo $telefone; ?>" ;
+				                env.contato = "<?php echo $contato; ?>" ;
+				                env.numero = "<?php echo $numero; ?>" ;
+				                env.complemento = "<?php echo $complemento; ?>" ;
+				                env.cep = "<?php echo $cep; ?>" ;
+				                env.bairro = "<?php echo $bairro; ?>" ;
+				                env.tipocadastro = "<?php echo $tipocadastro; ?>" ;
+	                
+								$.ajax({
+							            type: "POST",
+							            url: "../controller/ACAO/sl-ACAOcadusu.php",
+							            data: env,
+							            dataType : 'html',
+							            success: function(data){
+							               
+							                if(data.length > 0 && errocadastro == 0){
+							                	var form = {};
+						        				form.email = $('#email').val();
+						                         
+						    					window.location.href ="enviaemail.php?email="+env.email;
+							                }
+							            }, error: function(data) {
+							               
+							                alert("Erro, Houve uma falha ao salvar o registro!");
+							            }
+					    		});
+
+
 			            }
 	    		});
 
-
 	    		
+		    	
 
-		               	
-
-			
+				
 			});
+			
 
 		});				
 	</script>
