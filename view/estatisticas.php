1968a119093c3e46bfa0d5a42b8fdb31307daa97
Anderson Lima <andersoncardosolima0@gmail.com>
2015-12-15 19:41:57 -0200

    Adicionei cores nos jumbotrons

--- view/estatisticas.php
+++ view/estatisticas.php
@@ -33,7 +33,7 @@
 	    <div class="row">
 
 	    	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
-			        <div class="col-md-3 col-xs-3 col-lg-3"><div class="jumbotron">
+			        <div class="col-md-3 col-xs-3 col-lg-3"><div class="jumbotron" style="background-color: rgb(51,122,183);">
 			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
 			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
 			                    		<center><h4>Total de Cadastros:</h4></center>
@@ -45,7 +45,7 @@
 			                </div></div>
 			        </div></div>
 
-			        <div class="col-md-3 col-xs-3 col-lg-3"><div class="jumbotron">
+			        <div class="col-md-3 col-xs-3 col-lg-3"><div class="jumbotron" style="background-color: rgb(168,198,96);">
 			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
 			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
 			                    		<center><h4>Total de Cadastros por Categoria:</h4></center>
