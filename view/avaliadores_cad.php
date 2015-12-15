b868af8fc8e525838070faa8ca8c68262a2cf225
Anderson Lima <andersoncardosolima0@gmail.com>
2015-12-15 20:52:19 -0200

    Tela de cadastro de avaliadores

---
+++ view/avaliadores_cad.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
+    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
+    <script src="../js/jquery-2.1.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/tooltip.js"></script>
+    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
+    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+	<title>Avaliadores Cadastrados</title>
+</head>
+<body>
+	<div class="container">
+		<!--Banner-->
+	    <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
+	        <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
+	            <div class="col-md-12 col-xs-12 col-lg-12">
+	                <div class="col-md-4 col-xs-4 col-lg-4">
+	                    <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
+	                    <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
+	                </div>
+	                <div class="col-md-6 col-xs-6 col-lg-6">
+	                    <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Avaliadores Cadastrados</b></i></center></h1>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <!--Fim Banner-->
+
+	    <div class="jumbotron">
+	    	<div class="row">
+	    		<div class="col-md-12 col-xs-12 col-lg-12">
+	    			<center>
+		    			<table>
+		    				<tr><th>Nome</th>		<th>Cidade</th>		<th>Endereço</th>	<th>Função</th>	<th>Telefone</th>	<th>E-mail</th>	<th>Aprovado</th> <th>Reprovado</th></tr>
+		    				<tr><td>Anderson</td>	<td>Barrinha</td>	<td>Rua Marginal</td><td>Aluno</td>	<td>981334130</td>	<td>anderson@teste.com</td><td>sim</td><td>não</td></tr>
+		    			</table>
+		    		</center>
+	    		</div>
+	    	</div>
+	    </div>
+	</div><!--fim container-->
+</body>
+</html>
