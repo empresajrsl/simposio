9edbec21404683bd9f6d9c1648388b94a565c2af
Anderson Lima <andersoncardosolima0@gmail.com>
2015-12-15 17:34:31 -0200

    Ainda estou trabalhando nesta tela

---
+++ view/estatisticas.php
@@ -0,0 +1,97 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
+    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
+    <script src="../js/jquery-2.1.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/tooltip.js"></script>
+    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
+    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+	<title>Painel de Estatisticas</title>
+</head>
+<body>
+	<div class="container">
+		<!--Banner-->
+	    <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
+	        <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
+	            <div class="col-md-12 col-xs-12 col-lg-12">
+	                <div class="col-md-4 col-xs-4 col-lg-4">
+	                    <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
+	                    <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
+	                </div>
+	                <div class="col-md-6 col-xs-6 col-lg-6">
+	                    <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Painel de estatitisticas</b></i></center></h1>
+	                </div>
+	            </div>
+	        </div>
+	    </div>
+	    <!--Fim Banner-->
+
+    	<h3><center>Bem vindo ao painel de estatisticas.</center></h3>
+
+	    <div class="row">
+
+	    	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			        <div class="col-md-6 col-xs-6 col-lg-6"><div class="jumbotron">
+			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<center><h3>Total de Cadastros:</h3></center>
+			                    </div></div></br>
+
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<label>Total de Inscritos:</label> php</br>
+			                    		<label>Total de Artigos:</label> php</br>
+			                    		<label>Total de Artigos Aprovados:</label> php</br>
+			                    		<label>Total de Artigos Reprovados:</label> php</br>
+			                    		<label>Total de artigos Não Avaliados:</label> php</br>
+			                    		<label>Total de artigos Avaliados:</label> php</br>
+			                    </div></div>
+			                </div></div>
+			        </div></div>
+
+			        <div class="col-md-6 col-xs-6 col-lg-6"><div class="jumbotron">
+			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<center><h3>Total de Cadastros por Categoria:</h3></center>
+			                    </div></div></br>
+
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<label>Categoria 1:</label> php</br>
+			                    		<label>Categoria 1:</label> php</br>
+			                    		<label>Categoria 1:</label> php</br>
+			                    		<label>Categoria 1:</label> php</br>
+			                    </div></div>
+			                </div></div>
+			        </div></div>
+	    	</div></div>
+
+	    	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			        <div class="col-md-6 col-xs-6 col-lg-6"><div class="jumbotron">
+			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<center><h3>Total de Trabalhos Aprovados:</h3></center>
+			                    </div></div></br>
+
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    	teste	
+			                    </div></div>
+			                </div></div>
+			        </div></div>
+
+			        <div class="col-md-6 col-xs-6 col-lg-6"><div class="jumbotron">
+			               	<div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    		<center><h3>Total de Trabalhos Aprovados por Categoria:</h3></center>
+			                    </div></div></br>
+
+			                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+			                    	teste	
+			                    </div></div>
+			                </div></div>
+			        </div></div>
+			</div></div>
+		</div>
+	</div><!--fim container-->
+</body>
+</html>
