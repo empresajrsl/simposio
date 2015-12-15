1a546b574eab9d76c38915bbc7a8d2ec7dc7bf7f
Anderson Lima <andersoncardosolima0@gmail.com>
2015-12-15 16:19:31 -0200

    Modelo ainda inacabado do menu do adiministrador

---
+++ view/menuadm.php
@@ -0,0 +1,62 @@
+<html>
+<head>
+	<meta charset="utf-8">
+	<link type="text/css" rel="stylesheet" href="../css/bootstrap.min.css" ></link>
+    <link type="text/css" rel="stylesheet" href="../css/style.css" ></link>
+    <script src="../js/jquery-2.1.1.min.js"></script>
+    <script src="../js/bootstrap.min.js"></script>
+    <script src="../js/tooltip.js"></script>
+    <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
+    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+	<title>Painel do administrador</title>
+<body>
+	<div class="container">
+		<!--Banner-->
+        <div class="jumbotron" style="background-image: url('../images/fundo.png'); background-size: cover;">
+            <div class="row" style="margin-left: -6.5%; margin-right: 6.5%">
+                <div class="col-md-12 col-xs-12 col-lg-12">
+                    <div class="col-md-4 col-xs-4 col-lg-4">
+                        <img src="../images/SGAGRO LOGO.png" style="width: 40%; height: 15%;">
+                        <img src="../images/unesp.jpg" style="width: 40%; height: 15%; margin-left: 5%;">
+                    </div>
+                    <div class="col-md-6 col-xs-6 col-lg-6">
+                        <center><h1><i style="font-family: 'Plantagenet Cherokee';"><b>Painel do Administrador</b></i></center></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--Fim Banner-->
+        
+        <h3><center>Bem vindo ao painel de controle do sistema</center></h3>
+        
+        <div class="row">
+            <div class="col-md-4 col-xs-4 col-lg-4"><div class="jumbotron">
+                    <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                        <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                        <center><h2>Menu</h2></center>
+                        </div></div></br>
+
+                        <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                        <button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Estatisticas</button>
+                        </div></div></br>
+
+                        <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                        <button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Avaliadores</button>
+                        </div></div></br>
+
+                        <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                        <button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Novo Avaliador</button>
+                        </div></div></br>
+                    </div></div>
+            </div></div>
+
+            <div class="col-md-8 col-xs-8 col-lg-8"><div class="jumbotron"> 
+                <div class="row"><div class="col-md-12 col-xs-12 col-lg-12">
+                    <center><img src="../images/SGAGRO LOGO.png" style="width: 90%; height: 43%;"></center>
+                </div></div> 
+            </div></div>
+        </div>
+
+	</div><!--fim container-->
+</body>
+</html>
