927969e70c1686107dee0a712d2b27bb186ad574
Marlon <marlu_tcu@hotmail.com>
2015-12-17 18:06:06 -0300

    modificações no visual

--- view/menuadm.php
+++ view/menuadm.php
@@ -29,50 +29,47 @@
         
         <h3><center>Bem vindo ao painel de controle do sistema</center></h3>
         
-        <div class="row"><!--row nescessaria para deixar os jumbotrons alinhados com o banner-->
+        <div class="row" ><!--row nescessaria para deixar os jumbotrons alinhados com o banner-->
 
 
-            <div class="col-md-4 col-xs-4 col-lg-4"><!--jumbotron do menu-->
-                <div class="jumbotron">
+            <div class="col-md-2 col-xs-2 col-lg-2" ><!--jumbotron do menu-->
+                <div class="jumbotron" style="height:440px">
+                  
                     <div class="row">
-                        <div class="col-md-12 col-xs-12 col-lg-12">
-                            
-                            <div class="row">
-                                <div class="col-md-12 col-xs-12 col-lg-12">
-                                    <center><h2>Menu</h2></center>
-                                </div>
-                            </div></br>
-
-                            <div class="row">
-                                <div class="col-md-12 col-xs-12 col-lg-12">
-                                    <a href="estatisticas.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12"> Estatisticas</button></a>
-                                </div>
-                            </div></br>
-
-                            <div class="row">
-                                <div class="col-md-12 col-xs-12 col-lg-12">
-                                    <a href="avaliadores_cad.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Avaliadores</button></a>
-                                </div>
-                            </div></br>
-
-
-                            <div class="row">
-                                <div class="col-md-12 col-xs-12 col-lg-12">
-                                    <a href="cadadm.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Novo adminístrador</button></a>
-                                </div>
-                            </div></br>
                         
-                        </div>
-                    </div>
-                </div>
-            </div><!--fim do jumbotron do menu-->
+                            <center><h2>Menu</h2></center>
+                        
+                    </div></br>
+                    
+                    <div class="row">
+                        
+                            <center><a href="estatisticas.php"><button class="btn btn-primary  col-md-12 col-xs-12 col-lg-12" style="width:140px; margin-left:-34px" > Estatisticas</button></a></center>
+                        
+                    </div></br></br>
 
-            <div class="col-md-8 col-xs-8 col-lg-8"><div class="jumbotron"><!--jumbotron da imagem-->
-                <div class="row">
-                    <div class="col-md-12 col-xs-12 col-lg-12">
-                        <center><img src="../images/SGAGRO LOGO.png" style="width: 90%; height: 43%;"></center>
-                    </div>
-                </div> 
+                    <div class="row">
+                        
+                            <center><a href="avaliadores_cad.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12" style="width:140px; margin-left:-34px">Avaliadores</button></a></center>
+                        
+                    </div></br></br>
+
+
+                    <div class="row">
+                    
+                            <center><a href="cadadm.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12" style="width:140px; margin-left:-34px">Novo adminístrador</button></a></center>
+                      
+                    </div></br></br>
+                    
+                </div>
+            
+                
+            </div>
+
+            <div class="col-md-10 col-xs-10 col-lg-10"><div class="jumbotron"><!--jumbotron da imagem-->
+                
+                        <center><img src="../images/SGAGRO LOGO.png" style="width: 100%; height: 43%;"></center>
+                  
+                
             </div><!--jumbotron da imagem-->
 
         </div><!-- fim da row nescessaria para deixar esse jumbotron alinhado com o banner-->
