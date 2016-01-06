2c71a763afa2aa2a02d24b76c29a9332ac870c62
Marlon <marlu_tcu@hotmail.com>
2016-01-06 19:49:53 -0300

    modificações no visual

--- view/estatisticas.php
+++ view/estatisticas.php
@@ -10,11 +10,8 @@ $totalcad = $busca[0]['total'];
 $busca = select('count(id_usuario) as total','sl_cadusu','');
 $totalcad = $busca[0]['total'];
 
-
-
-
-
 ?> 
+
 <html>
 <head>
 	<meta charset="utf-8">
@@ -23,6 +20,7 @@ $totalcad = $busca[0]['total'];
     <script src="../js/jquery-2.1.1.min.js"></script>
     <script src="../js/bootstrap.min.js"></script>
     <script src="../js/tooltip.js"></script>
+    <script src="../Chart.min.js"> </script>
     <script src="../plugin/jquery-validate/jquery.validate.min.js"></script>
     <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
 	<title>Painel de Estatisticas</title>
@@ -45,44 +43,86 @@ $totalcad = $busca[0]['total'];
 	    </div>
 	    <!--Fim Banner-->
 
-    	<h3><center>Bem vindo ao painel de estatisticas.</center></h3>
+    	
+		<div class="row">
+			
+				<div class="panel panel-default" style="border-radius:30px">
+					<div class="panel-heading" style="background-color: black; padding:15px; border-radius:30px 30px 0px 0px"></div>
+					
+					<table style="margin-left: 100px">
+						<!-- <th> Total de usuarios cadastrados </th>
+						<th> Total de trabalhos cadastrados </th>
+						<th> Total de incrições pagas </th>
+						<th> Total trabalhos aprovados </th> -->
 
+						<td>
+							<img src="../images/digitos/d1.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d0.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d2.bmp" style="width:12%; heigth:12%;">
+						</td>
+
+						<td>		
+							<img src="../images/digitos/d2.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d6.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d3.bmp" style="width:12%; heigth:12%;">
+						</td>
+
+						<td>		
+							<img src="../images/digitos/d5.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d1.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d6.bmp" style="width:12%; heigth:12%;">
+						</td>
+
+						<td>		
+							<img src="../images/digitos/d3.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d0.bmp" style="width:12%; heigth:12%;">
+							<img src="../images/digitos/d2.bmp" style="width:12%; heigth:12%;">
+						</td>
+					</table>
+					
+				</div>
+		</div><br>	
 	    <div class="row">
 
-				<div class="col-md-3 col-xs-3 col-lg-3">
-		        	<div class="jumbotron" style="background-color: rgb(51,122,183);">
+				<!-- <div class="col-md-3 col-xs-3 col-lg-3">
+		        	<div class="jumbotron">
 		               	<center><h4>Total de Cadastros:</h4></center>
 	                    <?php echo '<h2><center>'.$totalcad.'</h2></center>' ?>
 	            	</div>
-	            </div>
+	            </div> -->
 		    
 
-		        <div class="col-md-3 col-xs-3 col-lg-3">
-		        	<div class="jumbotron" style="background-color: rgb(168,198,96);">
-		               	<center><h4>Total de trabalhos Cadastrados por Categoria:</h4></center>
-	            		<br>
+		        <div class="col-md-4 col-xs-4 col-lg-4">
+		        	<div class="jumbotron box-chart">
+		               	 <center><h4>Total de trabalhos cadastrados por categoria</h4><canvas id="GraficoDonut" style="width:100%;heigth:200px;"></canvas></center>
+	                </div>
+	        	</div>
+
+	        	 <div class="col-md-4 col-xs-4 col-lg-4">
+		        	<div class="jumbotron box-chart">
+		               	 <center><h4>Total de trabalhos cadastrados por area tematica</h4><canvas id="GraficoDonut2" style="width:100%;heigth:200px;"></canvas></center>
 	                </div>
 	        	</div>
 		          
 		<!--/div></div-->
 
 		<!--div class="row"><div class="col-md-12 col-xs-12 col-lg-12"-->
-		        <div class="col-md-3 col-xs-3 col-lg-3">
+		        <!-- <div class="col-md-3 col-xs-3 col-lg-3">
 		        	<div class="jumbotron">
 		               	<div class="col-md-12 col-xs-12 col-lg-12">
 	                		<center><h4>Total de Trabalhos Aprovados:</h4></center>
 	                    </div></br>
 	                    	teste	
 	                </div>
-	            </div>
+	            </div> -->
 		                
 
-		        <div class="col-md-3 col-xs-3 col-lg-3">
+		        <div class="col-md-4 col-xs-4 col-lg-">
 		        	<div class="jumbotron">
-	               		<div class="col-md-12 col-xs-12 col-lg-12">
-		                    <center><h4>Total de Trabalhos Aprovados por Categoria:</h4></center>
-	                    </div></br>
-	                    	teste	
+	               		
+		                    <center><h4>Total de trabalhos aprovados por categoria</h4><canvas id="GraficoDonut3" style="width:100%;heigth:200px;"></canvas></center>
+	                    
+	                    	
 	    	        </div>
 	    	    </div>
 			                
@@ -92,37 +132,63 @@ $totalcad = $busca[0]['total'];
 
 				<div class="col-md-6 col-xs-6 col-lg-6">
 			        <div class="jumbotron">
-			            <center><h4>Total de Cadastros por Região:</h4></center>
-			            teste	
-                    </div></br>
+			           <center><h4>Total de cadastro por região</h4><canvas id="GraficoBarra" style="width:100%"></canvas></center>
+			            	
+                    </div>
                     			
                 </div>
            
 			    <div class="col-md-6 col-xs-6 col-lg-6">
 		        	<div class="jumbotron">
-	               	    <center><h4>Total de Cadastros por Estado:</h4></center>
-	               	    teste	
+	               	    <center><h4>Total de cadastro por Estado</h4><canvas id="GraficoBarra2" style="width:100%;"></canvas></center>
+	               	   
 	                </div>
 	            </div>
         </div>
 
 
-		<div class="row">
+		<!-- <div class="row">
 			<div class="col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3 col-lg-6 col-lg-offset-3">
 			    <div class="jumbotron">
 			        <center><h4>Trabalhos aprovados com inscrições efetuadas e pagas:</h4></center>
 			        teste
                 </div>
             </div>
-        </div>
+        </div> -->
 
         <div class="row">
         	<div class="col-md-4 col-md-offset-4 col-xs-4 col-xs-offset-4 col-lg-4 col-lg-offset-4">
-			    	<button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Voltar</button>
+			    	<a href="menuadm.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12 glyphicon glyphicon-home"> Menu</button></a>
                 </div>
             </div>
         </div></br>
 			                
     <div><!--fim container-->
+
+    	
+
+<script src="../js/scripts/graficomodular.js"></script>
+<script src="../js/scripts/graficobarra.js"></script>
+<script type="text/javascript">
+		window.onload = function(){
+
+        var ctx = document.getElementById("GraficoDonut").getContext("2d");
+        var PizzaChart = new Chart(ctx).Doughnut(data, options);
+
+        var ctx = document.getElementById("GraficoDonut2").getContext("2d");
+        var PizzaChart = new Chart(ctx).Doughnut(data2, options);
+
+        var ctx = document.getElementById("GraficoDonut3").getContext("2d");
+        var PizzaChart = new Chart(ctx).Doughnut(data3, options);
+
+        var ctx = document.getElementById("GraficoBarra").getContext("2d");
+        var BarChart = new Chart(ctx).Bar(databarra, options);
+
+        var ctx = document.getElementById("GraficoBarra2").getContext("2d");
+        var BarChart = new Chart(ctx).Bar(databarra2, options);
+    }</script>
+
+
+
 </body>
 </html>
