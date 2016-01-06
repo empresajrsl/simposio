6ad79660722a0115dc5913b1a8a4c88ab89f2c73
Marlon <marlu_tcu@hotmail.com>
2016-01-06 19:50:26 -0300

    link botão voltar

--- view/avaliadores_cad.php
+++ view/avaliadores_cad.php
@@ -42,7 +42,7 @@
 
 	    	<div class="row">
 	        	<div class="col-md-4 col-md-offset-4 col-xs-4 col-xs-offset-4 col-lg-4 col-lg-offset-4">
-				    	<button class="btn btn-primary col-md-12 col-xs-12 col-lg-12">Voltar</button>
+				    	<a href="menuadm.php"><button class="btn btn-primary col-md-12 col-xs-12 col-lg-12 glyphicon glyphicon-home"> Menu</button></a>
 	                </div>
 	            </div>
         	</div>
