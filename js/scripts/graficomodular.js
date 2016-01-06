add7b073688106d7d3fa35fc69e6c83e2246dc33
Marlon <marlu_tcu@hotmail.com>
2016-01-06 19:51:56 -0300

    chart js  e imagem de digitos

---
+++ js/scripts/graficomodular.js
@@ -0,0 +1,72 @@
+
+
+    var options = {
+        responsive:true        
+    };
+
+    var data = [
+        {
+            value: 35,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Vermelho"
+        },
+        {
+            value: 50,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Azul"
+        },
+        {
+            value: 15,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Amarelo"
+        }
+    ]
+
+    var data2 = [
+        {
+            value: 45,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Vermelho"
+        },
+        {
+            value: 30,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Azul"
+        },
+        {
+            value: 25,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Amarelo"
+        }
+    ]
+
+    var data3 = [
+        {
+            value: 45,
+            color:"#F7464A",
+            highlight: "#FF5A5E",
+            label: "Vermelho"
+        },
+        {
+            value: 30,
+            color: "#46BFBD",
+            highlight: "#5AD3D1",
+            label: "Azul"
+        },
+        {
+            value: 25,
+            color: "#FDB45C",
+            highlight: "#FFC870",
+            label: "Amarelo"
+        }
+    ]
+    
+    
+
+   
