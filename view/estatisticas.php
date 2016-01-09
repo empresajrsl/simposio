14ddc48c3ec75b1860fc5cf8a82203835dabb739
kraoolyh <anderson.souza0909@gmail.com>
2016-01-09 19:58:25 -0200

    inclusão da pagina     <?php include('../controller/funcoes/graficobarra.php'); ?>

--- view/estatisticas.php
+++ view/estatisticas.php
@@ -13,16 +13,6 @@
     <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
 	<title>Painel de Estatisticas</title>
 
-	<style type="text/css"> 
-    .digitos{ 
-    	margin-left: 5px;
-    };
-
-    
-
-
-    </style>
-
 </head>
 <body>
 	<div class="container">
@@ -45,38 +35,38 @@
 	    <!--Fim Banner-->
     	
 
-		<div class="row">
+		<div class="row" >
 			<div class="row">
-        		<div class="col-md-2 col-md-offset-5 col-xs-2 col-xs-offset-5 col-lg-2 col-lg-offset-5">
-			    	<a href="menuadm.php"><button class="btn btn-success col-md-12 col-xs-12 col-lg-12 glyphicon glyphicon-home" style="border-radius:60px"> Menu</button></a>
+        		<div class="col-md-2 col-md-offset-5 col-xs-2 col-xs-offset-5 col-lg-2 col-lg-offset-5" style="background-color: #fff">
+			    	<a href="menuadm.php"><button class="btn btn-success col-md-12 col-xs-12 col-lg-12 glyphicon glyphicon-home" style="border-radius:0px 0px 30px 30px; margin-top:-30px"> Menu</button></a>
                 </div>
             </div></br>
 
 			<div class="col-md-12 col-xs-12 col-lg-12">
 				<div class="col-md-3 col-xs-3 col-lg-3">
 					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-					style="color: white; background-color: #5cb85c; padding:15px; border-radius:30px 30px 0px 0px">
+					style="color: white; background-color: #5cb85c  ; padding:15px; border-radius:30px 30px 0px 0px">
 						<b>Usuários cadastrados</b>
 					</center>
 				</div>
 
 				<div class="col-md-3 col-xs-3 col-lg-3">
 					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-					style="color: white; background-color: #5cb85c; padding:15px; border-radius:30px 30px 0px 0px">
+					style="color: white; background-color: #5cb85c  ; padding:15px; border-radius:30px 30px 0px 0px">
 						<b>Trabalhos cadastrados</b>
 					</center>
 				</div>
 
 				<div class="col-md-3 col-xs-3 col-lg-3">
 					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-					style="color: white; background-color: #5cb85c; padding:15px; border-radius:30px 30px 0px 0px">
+					style="color: white; background-color: #5cb85c  ; padding:15px; border-radius:30px 30px 0px 0px">
 						<b>Trabalho aprovados</b>
 					</center>
 				</div>
 
 				<div class="col-md-3 col-xs-3 col-lg-3">
 					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-					style="color: white; background-color: #5cb85c; padding:15px; border-radius:30px 30px 0px 0px">
+					style="color: white; background-color: #5cb85c  ; padding:15px; border-radius:30px 30px 0px 0px">
 						<b>Inscrições pagas</b>
 					</center>
 				</div>
@@ -84,7 +74,7 @@
 
 			<div class ="col-md-12 col-lg-12 col-xs-12"><!--ajuste-->
 				<div class="panel panel-default" style="border-radius:30px">
-					<div class="panel-heading" style="background-color: #5cb85c; padding:15px; border-radius:30px 30px 0px 0px"></div>
+					<div class="panel-heading" style="background-color: #5cb85c  ; padding:15px; border-radius:30px 30px 0px 0px"></div>
 					
 					<table> 
 						<tr>
@@ -116,69 +106,52 @@
 				</div>
 			</div><!--Ajuste-->
 		</div><br>
-
+<div class="jumbotron" style="background-color: #fff">
+	   <h3>	Grafico por Cadastro de Trabalhos </h3>
 	    <div class="row">
-
-				<!-- <div class="col-md-3 col-xs-3 col-lg-3">
-		        	<div class="jumbotron">
-		               	<center><h4>Total de Cadastros:</h4></center>
-	                    <?php echo '<h2><center>'.$totalcad.'</h2></center>' ?>
-	            	</div>
-	            </div> -->
-		    
 
 		        <div class="col-md-4 col-xs-4 col-lg-4">
 		        	<div class="jumbotron box-chart">
-		               	 <center><h4>Total de trabalhos cadastrados por categoria</h4><canvas id="GraficoDonut" style="width:100%;heigth:200px;"></canvas></center>
+		               	 <center><h4>Total de trabalhos cadastrados por categoria</h4><canvas id="GraficoDonut" style="width:100%;heigth:300px;"></canvas></center>
 	                </div>
 	        	</div>
 
 	        	 <div class="col-md-4 col-xs-4 col-lg-4">
 		        	<div class="jumbotron box-chart">
-		               	 <center><h4>Total de trabalhos cadastrados por area tematica</h4><canvas id="GraficoDonut2" style="width:100%;heigth:200px;"></canvas></center>
+		               	 <center><h4>Total de trabalhos cadastrados por Área temática</h4><canvas id="GraficoDonut2" style="width:100%;heigth:300px;"></canvas></center>
 	                </div>
 	        	</div>
-		          
-		<!--/div></div-->
-
-		<!--div class="row"><div class="col-md-12 col-xs-12 col-lg-12"-->
-		        <!-- <div class="col-md-3 col-xs-3 col-lg-3">
-		        	<div class="jumbotron">
-		               	<div class="col-md-12 col-xs-12 col-lg-12">
-	                		<center><h4>Total de Trabalhos Aprovados:</h4></center>
-	                    </div></br>
-	                    	teste	
-	                </div>
-	            </div> -->
+		 
 		                
 		        <div class="col-md-4 col-xs-4 col-lg-">
 		        	<div class="jumbotron">
 	               		
-		                    <center><h4>Total de trabalhos aprovados por categoria</h4><canvas id="GraficoDonut3" style="width:100%;heigth:200px;"></canvas></center>
+		                    <center><h4>Total de trabalhos aprovados por categoria<br/><br/></h4><canvas id="GraficoDonut3" style="width:100%;heigth:300px;"></canvas></center>
 	                    
 	                    	
 	    	        </div>
 	    	    </div>
 			                
 		</div>
-
+</div>
 		<div class="row">
-
-				<div class="col-md-6 col-xs-6 col-lg-6">
-			        <div class="jumbotron">
-			           <center><h4>Total de cadastro por região</h4><canvas id="GraficoBarra" style="width:100%"></canvas></center>
-			            	
-                    </div>
-                    			
+			<div class="col-md-12 col-xs-12 col-lg-12">
+		        <div class="jumbotron" style="background-color: #fff">
+		           <center><h4>Total de cadastro por região</h4><canvas id="GraficoBarra" style="width:100%"></canvas></center>
+		            	
                 </div>
-           
-			    <div class="col-md-6 col-xs-6 col-lg-6">
-		        	<div class="jumbotron">
-	               	    <center><h4>Total de cadastro por Estado</h4><canvas id="GraficoBarra2" style="width:100%;"></canvas></center>
-	               	   
-	                </div>
-	            </div>
+                			
+            </div>
         </div>
+
+         <div class="row">
+		    <div class="col-md-12 col-xs-12 col-lg-12">
+	        	<div class="jumbotron" style="background-color: #fff">
+               	    <center><h4>Total de cadastro por Estado</h4><canvas id="GraficoBarra2" style="width:100%;"></canvas></center>
+               	   
+                </div>
+            </div>
+    	</div>
 
 		<!-- <div class="row">
 			<div class="col-md-6 col-md-offset-3 col-xs-6 col-xs-offset-3 col-lg-6 col-lg-offset-3">
@@ -194,12 +167,11 @@
     	
 
     	
-    	
+    <?php include('../controller/funcoes/funcoesmysql.php');?>
     <?php include('../controller/funcoes/placar.php'); ?>
-
     <?php include('../controller/funcoes/graficomodular.php'); ?>
+    <?php include('../controller/funcoes/graficobarra.php'); ?>
 
-<script src="../js/scripts/graficobarra.js"></script>
 <script type="text/javascript">
 		window.onload = function(){
 
