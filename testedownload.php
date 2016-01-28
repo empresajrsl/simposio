db9399b39e90046bc4cceccb078d52a7a0202d18
Anderson Lima <andersoncardosolima0@gmail.com>
2016-01-28 11:03:03 -0200

    Fiz pequenos ajustes de identação, visual e no texto

--- testedownload.php
+++ testedownload.php
@@ -10,7 +10,7 @@
     <script src="js/jquery-2.1.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
     <?php include("controller/funcoes/funcoesmysql.php"); ?>
-    <link rel="shortcut icon" href="../images/SGAGRO LOGO.ico" type="image/x-icon"/>
+    <link rel="shortcut icon" href="images/SGAGRO LOGO.ico" type="image/x-icon"/>
 	<title>Visualizar Trabalho</title>
 </head>
 <body>
@@ -30,37 +30,43 @@
 	        </div>
 	    </div>
 	    <!--Fim Banner-->
-	<div class="row">
-		<h3><center> Para realizar o downlod do arquivo clique sobre o icone disquete localizado na parte superior do documento</center></h3>
-	
-	</div>    
-	<div class="row"><br>	    
-		<?php
-			
-			if(isset($_GET['id'])){
 
-				$id = $_GET['id'];
-				echo '
-				<div class="row">
-	 				<iframe  class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1"src="uploads/'.$id.'.pdf" height="1500"> </iframe>
-	 				</div>
-		 		<br/><br/>';
-			}else{		
-				echo'
-		
-	 				<div class="row">
-		 				<iframe  class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1"src="SGAgro Avaliacao de trabalhos.pdf" height="1500"> </iframe>
-		 				</div>
-		 			<br/><br/>';  
-		 	}			
-		?>
-	</div>
+		<div class="jumbotron" style="background-color: white">
+			<div class="row">
+				<h3 class="col-md-12 col-xs-12 col-lg-12">
+					<center>Para realizar o downlod do arquivo, clique sobre o icone disquete localizado na parte superior (Mozilla Firefox) ou inferior (Google Chrome) do documento.</center>
+				</h3>
+			</div>   
 
-	<div class="row"><br>
+			<div class="row"><br>	    
+				<?php
+					
+					if(isset($_GET['id'])){
+
+						$id = $_GET['id'];
+						echo '
+							<div class="row">
+				 				<iframe  class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1"src="uploads/'.$id.'.pdf" height="1500"> </iframe>
+				 			</div><br/><br/>'; 		
+					}
+					else{		
+						echo'
+			 				<div class="row">
+				 				<iframe  class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1"src="SGAgro Avaliacao de trabalhos.pdf" height="1500"> </iframe>
+				 				</div>
+				 			<br/><br/>';  
+				 		}			
+				?>
+			</div>
+
+			<div class="row"><br>
 				<center class="col-md-12 col-xs-12 col-lg-12">
-					<button type="button" id="voltar" name="voltar" class="col-md-3 col-md-offset-4 btn btn-warning" onclick="history.go(-1)">Voltar</button>
-					</center><br><br><br><br>
-	</div>
+					<button type="button" id="voltar" name="voltar" class="col-md-3 col-md-offset-4 btn btn-warning glyphicon glyphicon-backward" onclick="history.go(-1)"> Voltar</button>
+				</center>
+			</div>
+		</div>
+
+	</div><!-- fecha container-->
 
 </body>
 </html>      
