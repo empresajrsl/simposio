07a88f03a962389fda8bcbb75e301a55f71567c6
Marlon <marlu_tcu@hotmail.com>
2016-01-29 17:32:20 -0300

    faz o update na tabela no capo idavaliador de acordo com o avalidor escolhido

--- controller/ACAO/atribuirtrab.php
+++ controller/ACAO/atribuirtrab.php
@@ -2,11 +2,22 @@
 include ("../funcoes/conexao.php");
 include ("../funcoes/funcoesmysql.php");
 
-session_start();
-$id = $_SESSION['idusu'];
-$trabalho = select("*","sl_artigo","avaliador1 =  $id || avaliador2 = $id ");
+if(isset($_POST)){
+$id = $_POST['id'];
+$idtrab = $_POST['idtrab'];
+$avaliador = $_POST['avaliador'];
+$avaliador1  = array('avaliador' => 1 , );
+$avaliador2 = array('avaliador' => 2 , );
+
+	if($avaliador == 1){
+		updatemysql("id_avaliador1 = ".$id." ",'sl_artigo'," id_artigo = ".$idtrab." ");
+		$trabalho = select("nome","sl_cadusu","id_usuario = ".$id." ") + $avaliador1 ;
+	}else{
+		updatemysql("id_avaliador2 = ".$id." ",'sl_artigo'," id_artigo = ".$idtrab." ");
+		$trabalho = select("nome","sl_cadusu","id_usuario = ".$id." ") + $avaliador2 ;	
+	}
 
 echo json_encode($trabalho);
-
+}
 
 ?>
