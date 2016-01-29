97889151c7b593886f36ec3ea3d2ee0f8aa5ae4b
Marlon <marlu_tcu@hotmail.com>
2016-01-29 17:33:16 -0300

    busca todos trbalhos cadatrados

---
+++ controller/ACAO/buscatrabalhos.php
@@ -0,0 +1,17 @@
+<?php
+include ("../funcoes/conexao.php");
+include ("../funcoes/funcoesmysql.php");
+
+$trabalhos = select('id_artigo,id_avaliador1,id_avaliador2,titulo,area,instituicao,categoria','sl_artigo','');
+$avaliadores = select('nome,email,id_usuario,instituicao','sl_cadusu',"tipocadastro = 'Avaliador'  ");
+if($trabalhos){
+	$dados = array('trabalhos' => $trabalhos , 'avaliadores' => $avaliadores );
+ // echo'<pre>';
+ // print_r($dados);
+ // echo'</pre>';
+	
+	echo json_encode($dados);
+}else{
+
+}
+?>
