90eb8141ee17943503793f7aee454f16065b1f4c
Marlon <marlu_tcu@hotmail.com>
2016-02-01 17:55:45 -0300

    inseri area responsavel

--- view/cadusu.php
+++ view/cadusu.php
@@ -525,7 +525,7 @@
                                         '</select>'+
                                     '</div>');
             }else{
-                $('#divarea').fadeOut('');
+                $('#divarea').html('');
             }
         });
     });
