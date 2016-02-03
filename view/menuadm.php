d149cdee868314344762ad34aa8f06476b69f99f
Anderson Lima <andersoncardosolima0@gmail.com>
2016-02-03 21:37:52 -0200

    pequenas mudanças visuais mas deixei o anterior comentado para evitar problemas

--- view/menuadm.php
+++ view/menuadm.php
@@ -9,7 +9,26 @@
         }
 
     </style>
-		<nav class="navbar navbar-inverse">
+    
+    <div class="container-fluid">
+    <nav class="navbar navbar-inverse">
+            <div class="navbar-header">
+              <a class="navbar-brand" href="#" style="font-size:28px;color:white">SGAgro</a>
+            </div>
+            <ul class="nav navbar-nav">
+              <li><a href="estatisticas.php"><span class="glyphicon glyphicon-stats" ></span> Estatisticas</a></li>
+              <li><a href="avaliadores_cad.php"><span class="glyphicon glyphicon-education" ></span> Avaliadores cadastrados</a></li>
+              <li><a href="atribuiravaliador.php"><span class="glyphicon glyphicon-star" ></span> Atribuir avaliadores</a></li>
+              <li><a href="cadadm.php"><span class="glyphicon glyphicon-plus" ></span> Novo Administrador</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right"style="margin-right: 1%">
+              <li><a href="#"><span class="glyphicon glyphicon-user"></span> <?php echo $nome;  ?>  </a></li>
+              <li><a href="../sessao/fecharsessao.php"><span class="glyphicon glyphicon-off"></span> Sair</a></li>
+            </ul>
+        </nav>
+        </div>
+
+		<!--nav class="navbar navbar-inverse">
           <div class="container-fluid">
             <div class="navbar-header">
               <a class="navbar-brand" href="#" style="font-size:28px;color:white">SGAgro</a>
@@ -25,7 +44,7 @@
               <li><a href="../sessao/fecharsessao.php"><span class="glyphicon glyphicon-off"></span> Sair</a></li>
             </ul>
           </div>
-        </nav>
+        </nav-->
 
 
 
