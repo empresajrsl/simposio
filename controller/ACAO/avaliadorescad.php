847990c269abc5156157c2c5d681bfce9e0795b5
Marlon <marlu_tcu@hotmail.com>
2016-02-03 15:12:56 -0300

    mudei a tabela na busca de avalaidores

--- controller/ACAO/avaliadorescad.php
+++ controller/ACAO/avaliadorescad.php
@@ -2,7 +2,7 @@
 include ("../funcoes/conexao.php");
 include ("../funcoes/funcoesmysql.php");
 
-$avaliadores = select("*","sl_avaliador","");
+$avaliadores = select("*","sl_cadusu","tipocadastro ='Avaliador' OR tipocadastro ='Autor e avaliador'  ");
 
 echo json_encode($avaliadores);
 
