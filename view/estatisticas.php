892673b694fbcc1681b1ce4b16466bfca5a74941
Marlon <marlu_tcu@hotmail.com>
2016-02-04 19:25:05 -0300

    aumentei o tamanho da  div do grafico

--- view/estatisticas.php
+++ view/estatisticas.php
@@ -13,10 +13,10 @@
 	<title>Painel de Estatisticas</title>
 	<style type="text/css">
 		#grafico1{
-			height: 300px;
+			height: 350px;
 		}
 		#grafico2{
-			height: 300px;
+			height: 350px;
 		}
 		
 		
@@ -24,124 +24,145 @@
 
 </head>
 <body>
-	<div class="col-md-12" style="position: fixed; z-index: 9999;"><?php include("menuadm.php"); ?></div>
-	<br><br><br>
+
+<div class="col-md-12" style="position: fixed; z-index: 9999;"><?php include("menuadm.php"); ?></div>
+<br><br><br>
+	<div class="container-fluid">
+
+	<div class="container-fluid">
 	
-	<div class="container-fluid"><!--container0-->
+		
 
-		<div class="container-fluid"><!--container1-->
-	        <div class="row" >
-				
-				<div class="col-md-12 col-xs-12 col-lg-12">
-					<div class="col-md-3 col-xs-3 col-lg-3">
-						<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-						style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
-							<b>Usuários cadastrados</b>
-						</center>
-					</div>
-
-					<div class="col-md-3 col-xs-3 col-lg-3">
-						<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-						style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
-							<b>Trabalhos cadastrados</b>
-						</center>
-					</div>
-
-					<div class="col-md-3 col-xs-3 col-lg-3">
-						<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-						style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
-							<b>Trabalho aprovados</b>
-						</center>
-					</div>
-
-					<div class="col-md-3 col-xs-3 col-lg-3">
-						<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
-						style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
-							<b>Inscrições pagas</b>
-						</center>
-					</div>
+        <div class="row" >
+			
+			<div class="col-md-12 col-xs-12 col-lg-12">
+				<div class="col-md-3 col-xs-3 col-lg-3">
+					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
+					style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
+						<b>Usuários cadastrados</b>
+					</center>
 				</div>
 
-				<div class ="col-md-12 col-lg-12 col-xs-12"><!--ajuste-->
-					<div class="panel panel-default" style="border-radius:30px">
-						<div class="panel-heading" style="background-color: green ; padding:15px; border-radius:30px 30px 0px 0px"></div>
-						
-						<table> 
-							<tr>
-								<td class="col-md-3 col-xs-3 col-lg-3" >
-									<center id="usuarios">
-									
-									</center>
-								</td>
+				<div class="col-md-3 col-xs-3 col-lg-3">
+					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
+					style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
+						<b>Trabalhos cadastrados</b>
+					</center>
+				</div>
 
-								<td class="col-md-3 col-xs-3 col-lg-3">
-									<center id="trabalhos">
-									
-									</center>
-								</td>
+				<div class="col-md-3 col-xs-3 col-lg-3">
+					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
+					style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
+						<b>Trabalho aprovados</b>
+					</center>
+				</div>
 
-								<td class="col-md-3 col-xs-3 col-lg-3">
-									<center id="trabaprovado">
-									
-									</center>
-								</td>
+				<div class="col-md-3 col-xs-3 col-lg-3">
+					<center class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" 
+					style="color: white; background-color: green ; padding:15px; border-radius:30px 30px 0px 0px">
+						<b>Inscrições pagas</b>
+					</center>
+				</div>
+			</div>
 
-								<td class="col-md-3 col-xs-3 col-lg-3">
-									<center id="inscricao">
-									
-									</center>
-								</td>
-							</tr>
-						</table>
-					</div>
-				</div><!--Ajuste-->
-			</div><br>
-			
-			<div class="jumbotron" style="background-color: #fff">
-		    	<div class="row">
-			        <div class="col-md-4 col-xs-4 col-lg-4">
-			        	<div class="jumbotron box-chart">
-			               	<center><h4>Total de trabalhos cadastrados por categoria</h4><canvas id="GraficoDonut" ></canvas></center>
-		                </div>
-		        	</div>
+			<div class ="col-md-12 col-lg-12 col-xs-12"><!--ajuste-->
+				<div class="panel panel-default" style="border-radius:30px">
+					<div class="panel-heading" style="background-color: green ; padding:15px; border-radius:30px 30px 0px 0px"></div>
+					
+					<table> 
+						<tr>
+							<td class="col-md-3 col-xs-3 col-lg-3" >
+								<center id="usuarios">
+								
+								</center>
+							</td>
 
-		        	<div class="col-md-4 col-xs-4 col-lg-4">
-			        	<div class="jumbotron box-chart">
-			               	<center><h4>Total de trabalhos cadastrados por Área temática</h4><canvas id="GraficoDonut2" ></canvas></center>
-		                </div>
-		        	</div>
-			 
-			                
-			        <div class="col-md-4 col-xs-4 col-lg-">
-			        	<div class="jumbotron">
-		               	    <center><h4>Total de trabalhos aprovados por categoria</h4><br><canvas id="GraficoDonut3" ></canvas></center>    	
-		    	        </div>
-		    	    </div>
-		    	</div>
-		    </div>
+							<td class="col-md-3 col-xs-3 col-lg-3">
+								<center id="trabalhos">
+								
+								</center>
+							</td>
 
-			<div class="jumbotron" style="background-color: #fff">
-				<div class="row">
-			        <div class="col-md-12 col-xs-12 col-lg-12">
-			           <center>
-			           		<h4>Total de cadastro por</h4>
-			           		<select class="form-control" id="opcao">
-			           			<option id="regiao">Região</option>
-			           			<option id="estado">Estado</option>
-			           		</select>
-			           		<div class="row" id="grafico1">
-			           			<canvas id="GraficoBarra" height="80" ></canvas>
-			           		</div>
-			           		<div class="row" style="display:none" id="grafico2">
-			           			<canvas id="GraficoBarra2" height="80" ></canvas>
-			           		</div>	
-			           </center>   	
-	                </div>    			
-	            </div>
-	        </div>
-	    </div><!--container1-->
+							<td class="col-md-3 col-xs-3 col-lg-3">
+								<center id="trabaprovado">
+								
+								</center>
+							</td>
+
+							<td class="col-md-3 col-xs-3 col-lg-3">
+								<center id="inscricao">
+								
+								</center>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div><!--Ajuste-->
+		</div><br>
+
+   	</div>	
+		
+   	
+	    
+    	
+   	<div class="container-fluid">
+		
+<div class="jumbotron" style="background-color: #fff">
+	   
+	    <div class="row">
+
+		        <div class="col-md-4 col-xs-4 col-lg-4">
+		        	<div class="jumbotron box-chart">
+		               	 <center><h4>Total de trabalhos cadastrados por categoria</h4><canvas id="GraficoDonut" ></canvas></center>
+	                </div>
+	        	</div>
+
+	        	 <div class="col-md-4 col-xs-4 col-lg-4">
+		        	<div class="jumbotron box-chart">
+		               	 <center><h4>Total de trabalhos cadastrados por Área temática</h4><canvas id="GraficoDonut2" ></canvas></center>
+	                </div>
+	        	</div>
+		 
 		                
-    <div><!--fim container0-->
+		        <div class="col-md-4 col-xs-4 col-lg-">
+		        	<div class="jumbotron">
+	               		
+		                    <center><h4>Total de trabalhos aprovados por categoria</h4><canvas id="GraficoDonut3" ></canvas></center>
+	                    
+	                    	
+	    	        </div>
+	    	    </div>
+			                
+		</div>
+</div>
+		<div class="row">
+			<div class="col-md-12 col-xs-12 col-lg-12">
+		        <div class="jumbotron" style="background-color: #fff">
+		           <center>
+		           		<h4>Total de cadastro por</h4>
+		           		<select class="form-control" id="opcao">
+		           			<option id="regiao">Região</option>
+		           			<option id="estado">Estado</option>
+		           		</select>
+		           		<div class="row" id="grafico1">
+		           			<canvas id="GraficoBarra" height="80" ></canvas>
+		           		</div>
+		           		<div class="row" style="display:none" id="grafico2">
+		           			<canvas id="GraficoBarra2" height="80" ></canvas>
+		           		</div>	
+
+		           </center>
+		            	
+                </div>
+                			
+            </div>
+        </div>
+
+         
+
+		
+			                
+    <div><!--fim container-->
 
     	
 
