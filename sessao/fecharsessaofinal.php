025f9266805b6f92b30bb18faeecd8cf8267e497
Marlon <marlu_tcu@hotmail.com>
2016-02-04 18:19:45 -0300

    tive que apagar e colonar de novo pra poder postar as atualização e correções queeu fiz, pois estava dando problema com os merges

--- sessao/fecharsessaofinal.php
+++
@@ -1,7 +0,0 @@
-<?php
-	session_start(); // Inicia a sessão
-	session_destroy(); // Destrói a sessão limpando todos os valores salvos
-	
-	header("Location: ../view/fim.php"); exit; // Redireciona o visitante
-	
-?>
