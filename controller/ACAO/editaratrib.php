2e9dd994f0a5cef1ff186bef2ac3bee763d035c4
Marlon <marlu_tcu@hotmail.com>
2016-02-05 11:39:12 -0300

    correção da busca

--- controller/ACAO/editaratrib.php
+++ controller/ACAO/editaratrib.php
@@ -11,11 +11,11 @@ $avaliador2 = array('avaliador' => 2 , );
 
 	if($avaliador == 1){
 		updatemysql("id_avaliador1 = 0 ",'sl_artigo'," id_artigo = ".$idtrab." ");
-		$trabalho = select("nome,id_usuario","sl_cadusu"," tipocadastro = 'Avaliador' AND status = 1 ");
+		$trabalho = select("nome,id_usuario","sl_cadusu"," tipocadastro = 'Avaliador' OR tipocadastro = 'Autor e Avaliador' AND status = 1 ");
 		$dados = array('avaliadores' => $trabalho , 'avaliadorn' => $avaliador1 );
 	}else{
 		updatemysql("id_avaliador2 = 0 ",'sl_artigo'," id_artigo = ".$idtrab." ");
-		$trabalho = select("nome,id_usuario","sl_cadusu","tipocadastro = 'Avaliador' AND status = 1 ");
+		$trabalho = select("nome,id_usuario","sl_cadusu","tipocadastro = 'Avaliador' OR tipocadastro = 'Autor e Avaliador' AND status = 1 ");
 		$dados = array('avaliadores' => $trabalho , 'avaliadorn' => $avaliador2 );	
 	}
 
