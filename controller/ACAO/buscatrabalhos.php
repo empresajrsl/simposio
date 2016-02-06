dd47498f287684cf860734260630a28d11099f62
Marlon <marlu_tcu@hotmail.com>
2016-02-06 10:40:11 -0300

    correções

--- controller/ACAO/buscatrabalhos.php
+++ controller/ACAO/buscatrabalhos.php
@@ -3,7 +3,7 @@ include ("../funcoes/conexao.php");
 include ("../funcoes/funcoesmysql.php");
 
 $trabalhos = select('id_artigo,id_avaliador1,id_avaliador2,titulo,area,instituicao,categoria','sl_artigo','');
-$avaliadores = select('nome,email,id_usuario,instituicao','sl_cadusu',"tipocadastro = 'Avaliador' OR tipocadastro = 'Autor e Avaliador' AND status = 1  ");
+$avaliadores = select('nome,email,id_usuario,instituicao,status','sl_cadusu'," status = 1 AND tipocadastro = 'Avaliador' OR tipocadastro = 'Autor e Avaliador'    ");
 if($trabalhos){
 	$dados = array('trabalhos' => $trabalhos , 'avaliadores' => $avaliadores );
  // echo'<pre>';
