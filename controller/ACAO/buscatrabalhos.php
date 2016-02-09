7942ace9a1da6aa7b033d642377f488944a273db
Marlon <marlu_tcu@hotmail.com>
2016-02-09 13:07:44 -0300

    correção para so exibir trbalhos que ja foram enviado o arquivo

--- controller/ACAO/buscatrabalhos.php
+++ controller/ACAO/buscatrabalhos.php
@@ -2,7 +2,7 @@
 include ("../funcoes/conexao.php");
 include ("../funcoes/funcoesmysql.php");
 
-$trabalhos = select('id_artigo,id_avaliador1,id_avaliador2,titulo,area,instituicao,categoria','sl_artigo','');
+$trabalhos = select('id_artigo,id_avaliador1,id_avaliador2,titulo,area,instituicao,categoria','sl_artigo','idartigo <> "" ');
 $avaliadores = select('nome,email,id_usuario,instituicao,status','sl_cadusu'," status = 1 AND tipocadastro = 'Avaliador' OR tipocadastro = 'Autor e Avaliador'    ");
 if($trabalhos){
 	$dados = array('trabalhos' => $trabalhos , 'avaliadores' => $avaliadores );
