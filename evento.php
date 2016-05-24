9622e71457a8fe9eb1b81eb81079ee1dcbca1792
Marlon <marlu_tcu@hotmail.com>
2016-05-24 19:05:57 -0300

    inclusão do iframe com o pdf

--- evento.php
+++ evento.php
@@ -1,91 +1,79 @@
 <?php require_once('cabecalho.php') ?>
-
-<div id="main_content" class="row">
-
-	<div class="jumbotron text-justify">
-	<h3>Evento</h3><br>
-	<!--p> O Simpósio em Gestão do Agronegócio procura criar um espaço para difusão de conhecimento técnico-científico na área de Gestão do Agronegócio e constituir um ambiente que favoreça a formação da rede de relacionamento universidade-empresa, contribuindo para a formação de recursos humanos nas atividades empresariais do agronegócio.
-            O público alvo do evento são profissionais e pesquisadores na área do agronegócio e que demandam espaços de discussão de técnicas e processos de gestão que incrementem à competitividade das organizações pertencentes às cadeias produtivas do agronegócio.</p>
-
-    <p><br><h4>Local do evento:</h4>
-	Faculdade de Ciências Agrárias e Veterinárias - UNESP Jaboticabal<br>
-	Endereço: Via de acesso Prof. Paulo Donato Castellane s/n<br>
-	Jaboticabal - SP</p-->
-
-	<p>O Simpósio em Gestão do Agronegócio é organizado pelo Programa de Pós-
-	Graduação em Administração com área de concentração em Gestão de 
-	Organizações Agroindustriais junto a Faculdade de Ciências Agrárias e 
-	Veterinárias da Universidade Estadual Paulista (FCAV/UNESP).</p>
-
-	<p>Este evento tem como objetivo integrar a academia com o mercado a partir de 
-	um espaço para difusão de conhecimento técnico-científico na área de Gestão 
-	do Agronegócio e constituir um ambiente que favoreça a formação de redes de 
-	relacionamentos universidade-empresa, contribuindo para a formação de 
-	recursos humanos nas atividades empresariais do agronegócio.</p>
-
-	<p>O público alvo do evento compreende profissionais e pesquisadores na área do 
-	agronegócio que demandam fóruns para discussão de técnicas e processos de 
-	gestão que incrementem à competitividade das organizações pertencentes às 
-	cadeias produtivas do agronegócio.</p>
-
-	<p>Neste sentido, o evento combinará em 3 dias (08 a 10 de junho de 2016) 
-	múltiplos espaços para discussão e desenvolvimento na gestão do 
-	Agronegócio, onde destacamos: Palestras com profissionais e pesquisadores, 
-	apresentação de trabalhos científicos e tecnológicos e workshops em temas 
-	atuais no agronegócio (certificações, sustentabilidade, inovação, 
-	empreendedorismos, legislação ambiental e trabalhista, entre outras).</p>
-	<br>
-	<br>
-
-	<!-- <div class="row">
-		<div class="col-md-4">
-			<img src="images/logo50a.png"  width="420" height="350" style="margin-top:35px">
+<br>
+<br>
+<br>
+<div class="container-fluid">
+	<div id="main_content" class="row">
+		<div class="row" style="margin-bottom: 10px">
+		<iframe  class="col-md-offset-1 col-md-10" src="PROGRAMACAO DAS APRESENTACOES-Disponibilizada.pdf" height="1000"> </iframe>
 		</div>
-		<div class="col-md-4">
-			<img src="images/fachada.jpg"  width="420" height="350" style="margin-top:35px">
+
+			
+			<div class="jumbotron text-justify">
+				<br><br>
+				<h3>Evento</h3><br>
+				
+				<p>O Simpósio em Gestão do Agronegócio é organizado pelo Programa de Pós-
+				Graduação em Administração com área de concentração em Gestão de 
+				Organizações Agroindustriais junto a Faculdade de Ciências Agrárias e 
+				Veterinárias da Universidade Estadual Paulista (FCAV/UNESP).</p>
+
+				<p>Este evento tem como objetivo integrar a academia com o mercado a partir de 
+				um espaço para difusão de conhecimento técnico-científico na área de Gestão 
+				do Agronegócio e constituir um ambiente que favoreça a formação de redes de 
+				relacionamentos universidade-empresa, contribuindo para a formação de 
+				recursos humanos nas atividades empresariais do agronegócio.</p>
+
+				<p>O público alvo do evento compreende profissionais e pesquisadores na área do 
+				agronegócio que demandam fóruns para discussão de técnicas e processos de 
+				gestão que incrementem à competitividade das organizações pertencentes às 
+				cadeias produtivas do agronegócio.</p>
+
+				<p>Neste sentido, o evento combinará em 3 dias (08 a 10 de junho de 2016) 
+				múltiplos espaços para discussão e desenvolvimento na gestão do 
+				Agronegócio, onde destacamos: Palestras com profissionais e pesquisadores, 
+				apresentação de trabalhos científicos e tecnológicos e workshops em temas 
+				atuais no agronegócio (certificações, sustentabilidade, inovação, 
+				empreendedorismos, legislação ambiental e trabalhista, entre outras).</p>
+				<br>
+				<br>
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+			  <!-- Indicators -->
+			  <ol class="carousel-indicators">
+			    <li data-target="#myCarousel" data-slide-to="0"  class="active"></li>
+			    <li data-target="#myCarousel" data-slide-to="1"></li>
+			    <li data-target="#myCarousel" data-slide-to="2"></li>
+			  </ol>
+
+		  <!-- Wrapper for slides -->
+			  <div class="carousel-inner" role="listbox">
+			    <div class="item active">
+			     <center><img src="images/logo50a.png"  width="1100" height="500" ></center>
+			    </div>
+
+			    <div class="item">
+			      <center><img src="images/fachada.jpg"  width="1100" height="500" ></center>
+			    </div>
+
+			    <div class="item">
+			      <center><img src="images/unesp40a.png" width="350" height="500" ></center>
+			    </div>
+
+			    
+			  </div>
+
+		  <!-- Left and right controls -->
+			  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
+			    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+			    <span class="sr-only">Anterior</span>
+			  </a>
+		  	<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
+			    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+			    <span class="sr-only">Próximo</span>
+		  	</a>
 		</div>
-		<div class="col-md-4">
-			<img src="images/unesp40a.png"  width="400" height="420">
+
 		</div>
-	</div> -->
-
-	<div id="myCarousel" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#myCarousel" data-slide-to="0"  class="active"></li>
-    <li data-target="#myCarousel" data-slide-to="1"></li>
-    <li data-target="#myCarousel" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-     <center><img src="images/logo50a.png"  width="1100" height="500" ></center>
-    </div>
-
-    <div class="item">
-      <center><img src="images/fachada.jpg"  width="1100" height="500" ></center>
-    </div>
-
-    <div class="item">
-      <center><img src="images/unesp40a.png" width="350" height="500" ></center>
-    </div>
-
-    
-  </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Anterior</span>
-  </a>
-  <a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Próximo</span>
-  </a>
-</div>
-
-</div>
 </div>
 <br>
 <br>
