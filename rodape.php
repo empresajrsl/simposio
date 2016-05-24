7fd5866d5e4966d71fc84953cf399a01774313b5
Marlon <marlu_tcu@hotmail.com>
2016-05-24 11:44:06 -0300

    Atualização do rodapé

--- rodape.php
+++ rodape.php
@@ -1,3 +1,32 @@
 	</section>
+	<br><br>
+	<div class="container-fluid">
+		<hr style="color:#A8C660; background-color:#A8C660;">
+		<br>
+		<hr style="color:#A8C660; background-color:#A8C660;">
+		<br>
+		<div class="row" style="margin-left:40px">
+			<div class="col-md-12">
+				<div class="col-md-2">
+					<img src="logosrodape/unesplogo.jpg" style="width:35%; heigth:35%;">
+				</div>
+				<div class="col-md-2">
+					<img src="logosrodape/funeplogo.jpg" style="width:50%; heigth:50%; margin-top:15px">
+				</div>
+				<div class="col-md-2">
+					<img src="logosrodape/propg.jpg" style="width:30%; heigth:30%;">
+				</div>
+				<div class="col-md-2">
+					<img src="logosrodape/sociocanalogo.jpg" style="width:50%; heigth:50%; margin-top:25px">
+				</div>
+				<div class="col-md-2">
+					<img src="logosrodape/sigma.jpg" style="width:70%; heigth:70%; margin-top:10px">
+				</div>
+                                <div class="col-md-2">
+					<img src="logosrodape/Logo_PROEX.jpg" style="width:70%; heigth:70%;">
+				</div>
+			</div>
+		</div>
+	</div>
 </body>
 </html>
