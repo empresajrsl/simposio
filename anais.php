6d197acc4ae7e857337ad6fb4e5fb159d3ef241e
Anderson Lima <andersoncardosolima0@gmail.com>
2016-06-23 17:44:31 -0300

    Tela de pesquisa dos Anais

--- anais.php
+++ anais.php
@@ -2,8 +2,87 @@
 <?php require_once('cabecalho.php') ?>
 	
     <div class="container-fluid">
-     	<center><h1>2016</h1></center>
-     </div> 
+     	<!--center><h1>2016</h1></center-->
+     	<br>
+     
+     	<div class="col-md-8 col-md-offset-2">
+
+     		<form>
+     		<div class="row">
+     			<div class="col-md-3">
+     				<select class="form-control">
+     					<option>Autor ou Cooautor</option>
+     					<option>Titulo</option>
+     					<option>Palavras Chaves</option>
+     				</select>
+     			</div>
+
+     			<div class="col-md-6">
+     				<input type="text" name="pesquisa" class="form-control">
+     			</div>
+
+     			<div class="col-md-1">
+     				<button class="btn btn-success">Pesquisar</button>
+     			</div>
+     		</div>
+     		</form><br>
+
+     		<form>
+     		<div class="row">
+     			<div class="col-md-12">
+     				<LABEL>Selecione o ano do evento:</LABEL>
+     				<select class="form-control">
+     					<option>2015</option>
+     				</select>
+     			</div>
+     		</div><br>
+
+     		<div class="row">
+     			<div class="col-md-12">
+     				<label>Selecione a edição:</label>
+     				<select class="form-control">
+     					<option>Edição Completa - Volume 1.</option>
+     				</select>
+     			</div>
+     		</div><br>
+
+     		<div class="row">
+     			<div class="col-md-12">
+     				<label>Selecione a categoria:</label>
+     				<select class="form-control">
+     					<option>Artigos Completos.</option>
+     					<option>Relatos Técnicos.</option>
+     					<option>Resumo Expandido.</option>
+     				</select>
+     			</div>
+     		</div><br>
+
+     		<div class="row">
+     			<div class="col-md-12">
+     				<label>Selecione a Área:</label>
+     				<select class="form-control">
+     					<option>Desenvolvimento e Gestão.</option>
+     					<option>Economia e Finanças.</option>
+     					<option>Empreendedorismo, Inovação e Finanças.</option>
+     					<option>Estratégia, Planejamento e Governacia.</option>
+     					<option>Gestão de Pessoas e Estudos Organizacionais.</option>
+     					<option>Marketing e Mercados</option>
+     					<option>Logistica e Operações</option>
+     					<option>Sustentabilidade e Responsabilidade Socio Ambiental.</option>
+     				</select>
+     			</div>
+     		</div><br>
+
+     		<div class="row">
+     			<button type="button" class= "btn btn-success col-md-6 col-md-offset-3">
+     				Buscar
+     			</button>
+     			
+     		</div>
+     		</form>
+     	</div>
+
+    </div> 
     
 <?php require_once('rodape.php') ?>    
 
