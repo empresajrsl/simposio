df48c59f7fa897d1f7700eb0a9986e0e974d7f7d
Marlon <marlu_tcu@hotmail.com>
2016-06-23 21:50:59 -0300

    home

---
+++ home.php
@@ -0,0 +1,24 @@
+<?php require_once('cabecalho.php') ?>
+
+	<div id="main_content" class="row">
+		<div class="jumbotron text-justify">
+			<h3>Dicas para a apresentação do artigo e do relato técnico</h3>
+			<p>Caros participantes,</br>
+			Como sugestão, a apresentação do trabalho deve sintetizar os principais itens abordados no texto. Uma proposta para estruturar a apresentação é organizá-la nos seguintes itens: Contextualização, Objetivo, Metodologia, Análise e Considerações Finais. Ao final, também é necessário ressaltar as principais referências bibliográficas utilizadas na elaboração do trabalho.
+			Importante: O tempo máximo para a apresentação dos artigos e relatos técnicos é de 15 minutos, devendo ser finalizada, impreterivelmente, dentro do tempo estipulado.</p>
+			 
+			</br><h3>Dicas para a apresentação do painel</h3>
+			<p>Caros participantes,</br>
+			O painel deverá ser confeccionado pelos autores de cada trabalho seguindo o modelo disposto no link abaixo, obecendo as seguintes dimensões:</br>0,90m de largura  X 1,20m de altura<form action="downloadModelo.php"> <input type="submit" class="btn btn-info" id="download" name="download" value="Clique aqui para fazer download do modelo" ></span></p>
+
+			</br><h3>Programação das apresentações</h3>
+			<p>Caros participantes,</br>
+			Disponibilizamos abaixo a programação do evento em pdf para impressão ou a simples visualização dos horarios e datas das atividades.</p>
+		</div>
+
+		<div class="row" style="margin-bottom: 10px">
+			<iframe class="col-md-offset-1 col-md-10" src="PROGRAMACAO DAS APRESENTACOES-Disponibilizada.pdf" height="1000"></iframe>
+		</div>
+	</div>
+
+<?php require_once('rodape.php') ?>
