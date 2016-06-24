fcb85783a9cc4f237aae362cc075f3923cbae41c
Marlon <marlu_tcu@hotmail.com>
2016-06-23 21:50:43 -0300

    correção

--- expediente.php
+++ expediente.php
@@ -10,6 +10,14 @@
 			 <li><h4>Coordenação Geral: Prof. Dr. David Ferreira Lopes Santos</h4></li>
 			 <li><h4>Coordenação do Conselho Editorial: Profa. Dra. Lesley Carina do Lago Attadia Galli</h4></li>
 			 <li><h4>Conselho Editorial: Aba "Áreas", Prof. Dr. Adriano dos Reis Lucente e Profa. Dra. Sheila Farias Alves Garcia</h4></li>
+			 <li><h4>Prof. Dr. Roberto Louzada – UNESP/FCAV</h4></li> 
+			 <li><h4>Prof. Dr. Elton Eustaquio Casagrande – UNESP/FCLAR</h4></li>
+			 <li><h4>Profa. Dra. Sheila Farias Alves Garcia – UNESP/FCAV</h4></li> 
+			 <li><h4>Prof. Dr. Marcelo Giroto Rebelato – UNESP/FCAV</h4></li> 
+			 <li><h4>Profa. Dra. Andreia Marize Rodrigues – UNESP/FCAV</h4></li> 
+			 <li><h4>Prof. Dr. Leonardo Fernando Cruz Basso – Universidade Presbiteriana Mackenzie</h4></li> 
+			 <li><h4>Prof. Dr. Mário Sacomano Neto - UFSCar</h4></li>
+			 <li><h4>Prof. Dr. Silvio Carvalho Neto – Uni-FACEF</h4></li> 
 			 </ul>
 			 
 			 <h3>Periodicidade: Anual</h3>
