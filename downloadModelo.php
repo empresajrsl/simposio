dd19e3a47faa2b15def6513827a8ed9007361ace
Marlon <marlu_tcu@hotmail.com>
2016-06-23 21:52:28 -0300

    download modelo

---
+++ downloadModelo.php
@@ -0,0 +1,3 @@
+<iframe  class="col-md-10 col-md-offset-1 col-xs-10 col-xs-offset-1 col-lg-10 col-lg-offset-1" src="Modelo_Painel.doc" height="1000"> </iframe>
+
+<?php echo '<script>location.href="index.php";</script>'; ?>
