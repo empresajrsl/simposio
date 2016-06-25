465e1f5d28f9483946f66183e6f511ec986cd7ed
Anderson Lima <andersoncardosolima0@gmail.com>
2016-06-25 18:53:08 -0300

    Deixei o expediente da mesma forma que no servidor mas fiz a adequação das fontes

--- expediente.php
+++ expediente.php
@@ -1,30 +1,44 @@
 
 <?php require_once('estruturaHeader.php') ?>
 	
-<div class="container-fluid">
-	<div class="row" style="margin-left: 40px">
-		<div class="col-md-13">
+<!--div class="container-fluid"-->
+	<div id="main_content" class="row">
+		<div class="jumbotron text-justify">
 		 	 
-		 	 <h3>Corpo editorial </h3>
-		 	 <ul>   
-			 <li><h4>Coordenação Geral: Prof. Dr. David Ferreira Lopes Santos</h4></li>
-			 <li><h4>Coordenação do Conselho Editorial: Profa. Dra. Lesley Carina do Lago Attadia Galli</h4></li>
-			 <li><h4>Conselho Editorial: Aba "Áreas", Prof. Dr. Adriano dos Reis Lucente e Profa. Dra. Sheila Farias Alves Garcia</h4></li>
-			 <li><h4>Prof. Dr. Roberto Louzada – UNESP/FCAV</h4></li> 
-			 <li><h4>Prof. Dr. Elton Eustaquio Casagrande – UNESP/FCLAR</h4></li>
-			 <li><h4>Profa. Dra. Sheila Farias Alves Garcia – UNESP/FCAV</h4></li> 
-			 <li><h4>Prof. Dr. Marcelo Giroto Rebelato – UNESP/FCAV</h4></li> 
-			 <li><h4>Profa. Dra. Andreia Marize Rodrigues – UNESP/FCAV</h4></li> 
-			 <li><h4>Prof. Dr. Leonardo Fernando Cruz Basso – Universidade Presbiteriana Mackenzie</h4></li> 
-			 <li><h4>Prof. Dr. Mário Sacomano Neto - UFSCar</h4></li>
-			 <li><h4>Prof. Dr. Silvio Carvalho Neto – Uni-FACEF</h4></li> 
-			 </ul>
-			 
-			 <h3>Periodicidade: Anual</h3>
-			 <h3>Editor: Fundação de Apoio a Pesquisa, Ensino e Extensão – Funep</h3>
-			 <h3>Autor Corporativo: Faculdade de Ciências Agrárias e Veterinárias - FCAV</h3>
+		 	<p><b>Corpo Editorial do Simpósio em Gestão do Agronegócio:</b></p><br/>
+
+			<p><b>Comissão Organizadora:</b></p>
+			<p>Editora Chefe: Profa. Dra. Lesley Carina do Lago Attadia Galli</p>
+			<p>Profa. Dra. Andreia Marize Rodrigues – UNESP/FCAV</p>
+			<p>Prof. Dr. Adriano dos Reis Lucente – UNESP/FCAV</p>
+			<p>Prof. Dr. David Ferreira Lopes Santos – UNESP/FCAV</p>
+			<p>Prof. Dr. Elton Eustaquio Casagrande – UNESP/FCLAR</p>
+			<p>Prof. Dr. Leonardo Fernando Cruz Basso – Universidade Mackenzie</p>
+			<p>Prof. Dr. Mário Sacomano Neto - UFSCar</p>
+			<p>Prof. Dr. Marcelo Girotto Rebelato – UNESP/FCAV</p>
+			<p>Prof. Dr. Roberto Louzada – UNESP/FCAV</p>
+			<p>Profa. Dra. Sheila Farias Alves Garcia – UNESP/FCAV</p>
+			<p>Prof. Dr. Silvio Carvalho Neto – Uni-FACEF</p>
+			<p>Profa. Dra. Sheila Farias Alves Garcia – UNESP/FCAV</p>
+			<p>André Calabrez Hilf de Moraes</p>
+			<p>Bianca Rose Monteiro Bosco</p>
+			<p>Bruna Luisa da Silva</p>
+			<p>Cyro José Borges Soares</p>
+			<p>Diego Flores de Almeida</p>
+			<p>Georgia Saiani Mendes</p>
+			<p>Natália Beltrão Ruales</p>
+			<p>Pedro Ernesto Ruiz Bressan</p>
+			<p>Taisa Facioli Homem de Mello</p>
+			<p>Vitor Novelini Belotti</p><br/>
+
+			<p><b>Coordenação Geral do Evento:</b> Prof. Dr. David Ferreira Lopes Santos – UNESP/FCAV</p>
+			<p><b>Periodicidade do evento:</b> Anual.</p>
+			<p><b>Idiomas aceitos no evento:</b> Português, Espanhol e Inglês.</p>
+			<p><b>Editora:</b> FUNEP – Fundação de Apoio à Pesquisa e Extensão.</p>
+			<p><b>Autor Corporativo:</b> Faculdade de Ciências Agrárias e Veterinárias, Universidade Estadual Paulista “Júlio de Mesquita Filho”.</p>
+			<p><b>Via de Acesso:</b> Prof. Paulo Donato Castellane, S/N, Vila Industrial, Jaboticabal-SP. CEP: 14.884-900.</p>
 		</div>
 	</div>
-</div>
+<!--/div-->
     
-<?php require_once('rodape.php') ?>   
+<?php require_once('rodape.php') ?>
