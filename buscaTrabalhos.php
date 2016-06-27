69131e1939fc370ea0e82023bafdbee8b6b8447f
Marlon <marlu_tcu@hotmail.com>
2016-06-27 17:30:51 -0300

    coonsulta no banco de dados trabalhos e autores

--- buscaTrabalhos.php
+++ buscaTrabalhos.php
@@ -2,18 +2,24 @@
 include ("Simposio/submissao/controller/funcoes/funcoesmysql.php");
 
 
-$campos = "t.titulo,t.area,t.categoria,t.idartigo,t.instituicao,(SELECT CONCAT(nome,' ',sobrenome) FROM sl_cadusu WHERE id_usuario = g.id_usuario1) as autor_1,(SELECT CONCAT(nome,' ',sobrenome) FROM sl_cadusu WHERE id_usuario = g.id_usuario2) as autor_2, (SELECT CONCAT(nome,' ',sobrenome) FROM sl_cadusu WHERE id_usuario = g.id_usuario3) as autor_3, (SELECT CONCAT(nome,' ',sobrenome) FROM sl_cadusu WHERE id_usuario = g.id_usuario4) as autor_4,(SELECT CONCAT(nome,' ',sobrenome) FROM sl_cadusu WHERE id_usuario = g.id_usuario5) as autor_5 ";
-$tabela = "sl_artigo as t INNER JOIN sl_cadusu as c ON t.email = c.email INNER JOIN sl_grupo as g ON g.id_artigo = t.id_artigo";
+$campos = "*";
+$tabela = "trabautores";
 
-if(isset($_POST['titulo']) == false ){
-$condicao = "t.status = 1 AND t.avaliacao_concluida1 = 1 AND avaliacao_concluida2 = 1 ORDER BY t.titulo ";
-	
+if(isset($_POST['parametro']) ){
 
-}else{
+	$parametro = $_POST['parametro'];
+	$condicao = "titulo LIKE '%".$parametro."%' OR resumo LIKE '%".$parametro."%' OR autor_1 LIKE '%".$parametro."%' OR autor_2 LIKE '%".$parametro."%' OR autor_3 LIKE '%".$parametro."%' OR autor_4 LIKE '%".$parametro."%' OR autor_5 LIKE '%".$parametro."%' ORDER BY titulo";
 
-$titulo = $_POST['titulo'];
 
-$condicao = "t.status = 1 AND t.avaliacao_concluida1 = 1 AND avaliacao_concluida2 = 1 AND t.titulo LIKE '%".$titulo."%' ORDER BY t.titulo";
+}
+
+if(isset($_POST['form']) ){
+	$area = $_POST['area'];
+	$categoria = $_POST['categoria'];
+
+	$condicao = "area LIKE '%".$area."%' AND categoria LIKE  '%".$categoria."%' ORDER BY titulo ";
+
+
 
 }
 
@@ -24,4 +30,8 @@ echo json_encode($trabalhos);
 }else{
  echo "erro ao fazer a busca";
 }
+
+
+
+// OR autor_1 LIKE '%".$parametro."%' OR autor_2 LIKE '%".$parametro."%' OR autor_3 LIKE '%".$parametro."%' OR autor_4 LIKE '%".$parametro."%' OR autor_5 LIKE '%".$parametro."%'
 ?>
